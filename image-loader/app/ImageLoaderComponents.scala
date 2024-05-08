f485d93dd0bcc6ee483c5d06b039688f25a161de
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-05-08 11:38:56 +0100

    Everyone using GuardianUrlSchemeServices directly should take Services supplied by common config; no need to trouble yourselves with the details of how those urls are defined.

--- image-loader/app/ImageLoaderComponents.scala
+++ image-loader/app/ImageLoaderComponents.scala
@@ -1,6 +1,5 @@
 import com.gu.mediaservice.GridClient
 import com.gu.mediaservice.lib.aws.{Bedrock, S3Vectors, SimpleSqsMessageConsumer, Embedder}
-import com.gu.mediaservice.lib.config.{GuardianUrlSchemeServices}
 import com.gu.mediaservice.lib.imaging.ImageOperations
 import com.gu.mediaservice.lib.logging.GridLogging
 import com.gu.mediaservice.lib.management.InnerServiceStatusCheckController
@@ -45,8 +44,7 @@ class ImageLoaderComponents(context: Context) extends GridComponents(context, ne
     new QuarantineUploader(new QuarantineStore(config), config)
   )
 
-  val services = new GuardianUrlSchemeServices(config.domainRoot, config.serviceHosts, Set.empty)
-  private val gridClient = GridClient(services)(wsClient)
+  private val gridClient = GridClient(config.services)(wsClient)
 
   val metrics = new ImageLoaderMetrics(config, actorSystem, applicationLifecycle)
 
