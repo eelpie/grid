dec76497f4d67bbc5c95b862ceb3f9442ea1c755
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-05-14 17:16:19 +0100

    Delete unused config requests in service configs.
    Reduces the minimum startup footprint.

--- metadata-editor/app/lib/EditsConfig.scala
+++ metadata-editor/app/lib/EditsConfig.scala
@@ -1,14 +1,9 @@
 package lib
 
-import com.amazonaws.regions.{Region, RegionUtils}
 import com.gu.mediaservice.lib.config.{CommonConfig, GridConfigResources}
 
 
 class EditsConfig(resources: GridConfigResources) extends CommonConfig(resources) {
-  val dynamoRegion: Region = RegionUtils.getRegion(string("aws.region"))
-
-  val collectionsBucket: String = string("s3.collections.bucket")
-
   val editsTable = string("dynamo.table.edits")
   val editsTablePhotoshootIndex = string("dynamo.globalsecondaryindex.edits.photoshoots")
   val syndicationTable = string("dynamo.table.syndication")
@@ -16,6 +11,4 @@ class EditsConfig(resources: GridConfigResources) extends CommonConfig(resources
   val queueUrl = string("indexed.images.sqs.queue.url")
 
   val rootUri: String = services.metadataBaseUri
-  val kahunaUri: String = services.kahunaBaseUri
-  val loginUriTemplate: String = services.loginUriTemplate
 }
