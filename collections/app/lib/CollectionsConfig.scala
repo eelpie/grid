87477185a0f8b86bd0bf694c03e0a13e5e067084
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-05-16 11:55:12 +0100

    loader, projection, usages, collections and leases move to vhost urls.

--- collections/app/lib/CollectionsConfig.scala
+++ collections/app/lib/CollectionsConfig.scala
@@ -1,11 +1,12 @@
 package lib
 
 import com.gu.mediaservice.lib.config.{CommonConfig, GridConfigResources}
+import play.api.mvc.RequestHeader
 
 
 class CollectionsConfig(resources: GridConfigResources) extends CommonConfig(resources) {
   val collectionsTable = string("dynamo.table.collections")
   val imageCollectionsTable = string("dynamo.table.imageCollections")
 
-  val rootUri = services.collectionsBaseUri
+  val rootUri: RequestHeader => String = services.collectionsBaseUri
 }
