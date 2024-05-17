ac59f78d5fba2328fe9aea721bef21c06e2c270e
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-05-17 09:46:43 +0100

    Add instance field to ImageMessage so thrall knows the instance of an uploaded image to index.
    Very nice light touch in image uploader but
    extends UpdateMessage forces this to be dealt with for all update points immediately.

--- image-loader/app/model/Uploader.scala
+++ image-loader/app/model/Uploader.scala
@@ -390,7 +390,7 @@ class Uploader(val store: ImageLoaderStore,
 
     for {
       imageUpload <- fromUploadRequest(uploadRequest)
-      updateMessage = UpdateMessage(subject = Image, image = Some(imageUpload.image))
+      updateMessage = UpdateMessage(subject = Image, image = Some(imageUpload.image), instance = uploadRequest.instance)
       _ <- Future { notifications.publish(updateMessage) }
       // TODO: centralise where all these URLs are constructed
     } yield UploadStatusUri(s"${config.rootUri(request)}/uploadStatus/${uploadRequest.imageId}")
@@ -407,7 +407,7 @@ class Uploader(val store: ImageLoaderStore,
     imageWithUserEditsApplied <- ImageDataMerger.aggregate(imageWithoutUserEdits, gridClient, onBehalfOfFn)
     _ <- Future {
       notifications.publish(
-        UpdateMessage(subject = Image, image = Some(imageWithUserEditsApplied))
+        UpdateMessage(subject = Image, image = Some(imageWithUserEditsApplied), instance = uploadRequest.instance)
       )
     }
   } yield ()
