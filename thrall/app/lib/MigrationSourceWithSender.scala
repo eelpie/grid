f49ab4272070c437a048f1b5fffb38c60c9c6316
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-06-04 21:34:35 +0100

    Nerf the thrall migration thread; very noisy.

--- thrall/app/lib/MigrationSourceWithSender.scala
+++ thrall/app/lib/MigrationSourceWithSender.scala
@@ -36,7 +36,7 @@ object MigrationSourceWithSender extends GridLogging {
     // emits MigrationRequest
     val scrollingIdsSource =
       Source.repeat(())
-        .throttle(1, per = 1.second)
+        .throttle(1, per = 1.minute)
         .statefulMapConcat(() => {
           // This Akka-provided stage is explicitly provided as a way to safely wrap around mutable state.
           // Required here to keep a marker of the current search scroll. Scrolling prevents the
