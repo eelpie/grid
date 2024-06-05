39cd35e696f7e695c9e69e68b989f7cf69173e15
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-06-05 11:22:48 +0100

    Instance allowed check can almost certainly move to the auth call. auth becomes Are you authenticated and a member of this instance.
    Work around for not wanting to block the unauthed /auth/* end points and permissions not been invoked for every end point.

--- rest-lib/src/main/scala/com/gu/mediaservice/lib/auth/Authentication.scala
+++ rest-lib/src/main/scala/com/gu/mediaservice/lib/auth/Authentication.scala
@@ -73,8 +73,21 @@ class Authentication(config: CommonConfig,
 
   override def invokeBlock[A](request: Request[A], block: Authentication.Request[A] => Future[Result]): Future[Result] = {
     authenticationStatus(request) match {
-      // we have a principal, so process the block
-      case Right(principal) => block(new AuthenticatedRequest(principal, request))
+      case Right(principal) => {
+        // we have a principal, so process the block if the instance matches
+        val instance = instanceOf(request)
+        logger.info(s"Checking that $principal is allowed to access instanc $instance")
+        // Use the cookie instances for now but we are in a Future so are able to call the instances service for a canonical answer if we need to
+        val principalsInstances = principal.attributes.get(KindeAuthenticationProvider.instancesTypedKey).getOrElse(Seq.empty)
+        if (principalsInstances.contains(instance.id)) {
+          logger.debug("Allowing this request!")
+          block(new AuthenticatedRequest(principal, request))
+
+        } else {
+          logger.warn(s"Blocking request ${request.path} on instance $instance")
+          Future.successful(Forbidden("You do not have permission to use this instance"))
+        }
+      }
       // no principal so return a result which will either be an error or a form of redirect
       case Left(result) => result
     }
