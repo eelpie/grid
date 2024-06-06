82f0d1548eda31fb912f47efca685fd5179900e1
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-06-06 10:16:23 +0100

    Allow InnerServicePrincipal to access all instances.
    ie. For initial root collection setup.

--- rest-lib/src/main/scala/com/gu/mediaservice/lib/auth/Authentication.scala
+++ rest-lib/src/main/scala/com/gu/mediaservice/lib/auth/Authentication.scala
@@ -79,20 +79,27 @@ class Authentication(config: CommonConfig,
   override def invokeBlock[A](request: Request[A], block: Authentication.Request[A] => Future[Result]): Future[Result] = {
     // gracePeriodCountsAsAuthenticated is set to true here, so requests using this block should accept users whose session is in the grace period
     authenticationStatus(request, gracePeriodCountsAsAuthenticated = true) match {
-      // we have a principal, so process the block if the instance matches
       case Right(principal) => {
-        val instance = instanceOf(request)
-        logger.info(s"Checking that $principal is allowed to access instance $instance")
-        // Use the cookie instances for now but we are in a Future so are able to call the instances service for a canonical answer if we need to
-        val principalsInstances = principal.attributes.get(KindeAuthenticationProvider.instancesTypedKey).getOrElse(Seq.empty)
-        if (principalsInstances.contains(instance.id)) {
-          logger.debug("Allowing this request!")
-          block(new AuthenticatedRequest(principal, request))
-            .map(result => result.addAttr(RequestLoggingFilter.requestPrincipal, principal))
+        principal match {
+          case innerServicePrincipal: InnerServicePrincipal =>
+            logger.info("Allowing InnerServicePrincipal request for all instances")
+            block(new AuthenticatedRequest(innerServicePrincipal, request))
+              .map(result => result.addAttr(RequestLoggingFilter.requestPrincipal, principal))
 
-        } else {
-          logger.warn(s"Blocking request ${request.path} on instance $instance")
-          Future.successful(Forbidden("You do not have permission to use this instance"))
+          case _ =>
+            // we have an end user principal, so only process the block if the instance is allowed
+            val instance = instanceOf(request)
+            logger.info(s"Checking that $principal is allowed to access instanc $instance")
+            // Use the cookie instances for now but we are in a Future so are able to call the instances service for a canonical answer if we need to
+            val principalsInstances = principal.attributes.get(KindeAuthenticationProvider.instancesTypedKey).getOrElse(Seq.empty)
+            if (principalsInstances.contains(instance.id)) {
+              logger.debug("Allowing this request!")
+              block(new AuthenticatedRequest(principal, request))
+
+            } else {
+              logger.warn(s"Blocking request ${request.path} on instance $instance")
+              Future.successful(Forbidden("You do not have permission to use this instance"))
+            }
         }
       }
       // no principal so return a result which will either be an error or a form of redirect
