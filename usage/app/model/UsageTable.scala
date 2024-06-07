ee160ee8ed08a93177c1dc7c252dccdea9b952fe
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-06-07 11:10:44 +0100

    Marking TODO - Usages not instance aware.

--- usage/app/model/UsageTable.scala
+++ usage/app/model/UsageTable.scala
@@ -21,7 +21,7 @@ class UsageTable(
                                   client: AmazonDynamoDBAsync,
                                   client2: DynamoDbClient,
                                   tableName: String
-                                ) extends DynamoDB[MediaUsage](client, client2, tableName) with GridLogging {
+                                ) extends DynamoDB[MediaUsage](client, client2, tableName) with GridLogging { // TODO Not instane aware!
 
   val hashKeyName = "grouping"
   val rangeKeyName = "usage_id"
