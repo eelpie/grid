6f33e6daab75fdb2b32e09ab528283d4af72080c
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-06-23 22:29:44 +0100

    Push instance all the way down to fileKeyFromId to catch all usages.
    Delete image was not instance aware; needs to be pushed further down to the s3 key function?

--- thrall/app/controllers/ReaperController.scala
+++ thrall/app/controllers/ReaperController.scala
@@ -153,9 +153,9 @@ class ReaperController(
 
     (for {
       BatchDeletionIds(esIds, esIdsActuallyDeleted) <- es.hardDeleteNextBatchOfImages(isReapable, count, config.hardReapImagesAge)
-      mainImagesS3Deletions <- store.deleteOriginals(esIdsActuallyDeleted)
-      thumbsS3Deletions <- store.deleteThumbnails(esIdsActuallyDeleted)
-      pngsS3Deletions <- store.deletePNGs(esIdsActuallyDeleted)
+      mainImagesS3Deletions <- store.deleteOriginals(esIdsActuallyDeleted, i)
+      thumbsS3Deletions <- store.deleteThumbnails(esIdsActuallyDeleted, i)
+      pngsS3Deletions <- store.deletePNGs(esIdsActuallyDeleted, i)
       idsNotProcessedInDynamo <- softDeletedMetadataTable.clearStatuses(esIdsActuallyDeleted)
     } yield {
       metrics.hardReaped.increment(n = esIdsActuallyDeleted.size).run
@@ -163,9 +163,9 @@ class ReaperController(
         val wasHardDeletedFromES = esIdsActuallyDeleted.contains(id)
         val detail = Map(
           "ES" -> Some(wasHardDeletedFromES),
-          "mainImage" -> mainImagesS3Deletions.get(ImageIngestOperations.fileKeyFromId(id)),
-          "thumb" -> thumbsS3Deletions.get(ImageIngestOperations.fileKeyFromId(id)),
-          "optimisedPng" -> pngsS3Deletions.get(ImageIngestOperations.optimisedPngKeyFromId(id, instance.id)),
+          "mainImage" -> mainImagesS3Deletions.get(ImageIngestOperations.fileKeyFromId(id, instance)),
+          "thumb" -> thumbsS3Deletions.get(ImageIngestOperations.fileKeyFromId(id, instance)),
+          "optimisedPng" -> pngsS3Deletions.get(ImageIngestOperations.optimisedPngKeyFromId(id, instance)),
           "dynamo.table.softDelete.metadata" -> (if(wasHardDeletedFromES) Some(!idsNotProcessedInDynamo.contains(id)) else None)
         )
         logger.info(s"Hard deleted image $id : $detail")
