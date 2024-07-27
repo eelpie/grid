22fa891d05859c96cd296b401d79a580632eccd3
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-07-27 20:30:28 +0100

    Use image.id is the fallback content-disposition filename for better caching.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/aws/ContentDisposition.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/aws/ContentDisposition.scala
@@ -1,10 +1,9 @@
 package com.gu.mediaservice.lib.aws
 
 import com.gu.mediaservice.lib.logging.GridLogging
-import com.gu.mediaservice.model.{Asset, Crop, Image, ImageType, OptimisedPng, Source, Thumbnail}
+import com.gu.mediaservice.model._
 
 import java.net.URLEncoder
-import java.nio.charset.{Charset, StandardCharsets}
 
 trait ContentDisposition extends GridLogging {
 
@@ -17,7 +16,7 @@ trait ContentDisposition extends GridLogging {
     val filenameSuffix: String = s"(${image.id})$cropId$dimensions$extension"
     val baseFilename = getBaseFilename(image, filenameSuffix)
 
-    getContentDisposition(baseFilename)
+    getContentDisposition(baseFilename, image.id)
   }
 
   def getContentDisposition(image: Image, imageType: ImageType): String = {
@@ -30,7 +29,7 @@ trait ContentDisposition extends GridLogging {
     val filenameSuffix: String = s"(${image.id})$extension"
     val baseFilename = getBaseFilename(image, filenameSuffix)
 
-    getContentDisposition(baseFilename)
+    getContentDisposition(baseFilename, image.id)
   }
 
   private def getBaseFilename(image: Image, filenameSuffix: String): String = image.uploadInfo.filename match {
@@ -39,12 +38,12 @@ trait ContentDisposition extends GridLogging {
     case _ => filenameSuffix
   }
 
-  private def getContentDisposition(filename: String): String = {
+  private def getContentDisposition(filename: String, fallback: String): String = {
     // use both `filename` and `filename*` parameters for compatibility with user agents not implementing RFC 5987
     // they'll fallback to `filename`, which will be a UTF-8 string decoded as Latin-1 - this is a rubbish string, but only rubbish browsers don't support RFC 5987 (IE8 back)
     // See http://tools.ietf.org/html/rfc6266#section-5
-    val filenameISO = encodedFilename(StandardCharsets.ISO_8859_1, filename)
-    val filenameUTF8 = encodedFilename(StandardCharsets.UTF_8, filename)
+    val filenameISO = fallback
+    val filenameUTF8 = encodedFilenameUTF8(filename)
     s"""attachment; filename="$filenameISO"; filename*=UTF-8''$filenameUTF8"""
   }
 
@@ -61,13 +60,10 @@ trait ContentDisposition extends GridLogging {
     regex.replaceAllIn(filename, "")
   }
 
-
-  private def encodedFilename(charset: Charset, baseFilename: String): String = charset.displayName() match {
-    case "UTF-8" =>
+  private def encodedFilenameUTF8(filename: String) = {
       // URLEncoder converts ` ` to `+`, replace it with `%20`
       // See http://docs.oracle.com/javase/6/docs/api/java/net/URLEncoder.html
-      URLEncoder.encode(baseFilename, "UTF-8").replace("+", "%20")
-    case characterSet => baseFilename.getBytes(characterSet).toString
+      URLEncoder.encode(filename, "UTF-8").replace("+", "%20")
   }
 
 }
