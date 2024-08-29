8b2bee67af21c891786f2cccee8220f905dd12e3
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-08-29 20:02:56 +0100

    imgproxy is vhost based.

--- media-api/app/controllers/MediaApi.scala
+++ media-api/app/controllers/MediaApi.scala
@@ -86,7 +86,7 @@ class MediaApi(
   }
 
   private def searchLink()(implicit instance: Instance) = {
-    Link("search", searchLinkHref()(instance))
+    Link("search", searchLinkHref())
   }
 
   private def indexResponse(user: Principal)(implicit instance: Instance) = {
@@ -468,6 +468,7 @@ class MediaApi(
   }
 
   def downloadOptimisedImage(id: String, width: Integer, height: Integer, quality: Integer) = auth.async { implicit request =>
+    implicit val instance: Instance = instanceOf(request)
     implicit val logMarker: LogMarker = MarkerMap(
       "requestType" -> "download-optimised-image",
       "requestId" -> RequestLoggingFilter.getRequestId(request),
@@ -491,7 +492,7 @@ class MediaApi(
         }
 
         Future.successful(
-          Redirect(config.imgopsUri + List(sourceImageUri.getPath, sourceImageUri.getRawQuery).mkString("?") + s"&w=$width&h=$height&q=$quality")
+          Redirect(config.imgopsUri(instance) + List(sourceImageUri.getPath, sourceImageUri.getRawQuery).mkString("?") + s"&w=$width&h=$height&q=$quality")
         )
       }
       case _ => Future.successful(ImageNotFound(id))
