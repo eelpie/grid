8df7f38706c5123ccc6b51a37a19f3a10dbd9fcc
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-11-10 15:05:08 +0000

    Patch up soft deletes table best we can given that Scanmomo doesn't support composite keys on batch and there is no longer any error feedback now BatchWriteItemResult is missing

--- media-api/app/controllers/MediaApi.scala
+++ media-api/app/controllers/MediaApi.scala
@@ -290,6 +290,7 @@ class MediaApi(
   }
 
   def getSoftDeletedMetadata(id: String) = auth.async { request =>
+    implicit val instance: Instance = instanceOf(request)
     implicit val logMarker: LogMarker = MarkerMap(
       "requestType" -> "get-soft-deleted-metadata",
       "requestId" -> RequestLoggingFilter.getRequestId(request),
@@ -378,7 +379,7 @@ class MediaApi(
         if (imageCanBeDeleted){
           val canDelete = authorisation.isUploaderOrHasPermission(request.user, image.uploadedBy, DeleteImagePermission)
           if(canDelete){
-            val imageStatusRecord = ImageStatusRecord(id, request.user.accessor.identity, DateTime.now(DateTimeZone.UTC).toString, true)
+            val imageStatusRecord = ImageStatusRecord(id, request.user.accessor.identity, DateTime.now(DateTimeZone.UTC).toString, isDeleted = true, instance.id)
             softDeletedMetadataTable.setStatus(imageStatusRecord)
             .map { _ =>
               messageSender.publish(
