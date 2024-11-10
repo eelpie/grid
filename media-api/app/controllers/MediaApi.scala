68544d207cbca5ef57b39c67652d3004102251be
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-11-10 12:00:18 +0000

    !!!!!!!!!!!!!!!!! Start of multi tenant - Redefine Service.apiUrl to be a function of instance. Work through the usages.
    Map apiBaseUrl to request instance.
    
    Extract vhost service name function.

--- media-api/app/controllers/MediaApi.scala
+++ media-api/app/controllers/MediaApi.scala
@@ -7,6 +7,7 @@ import com.gu.mediaservice.lib.auth.Authentication._
 import com.gu.mediaservice.lib.auth.Permissions.{ArchiveImages, DeleteCropsOrUsages, EditMetadata, UploadImages, DeleteImage => DeleteImagePermission}
 import com.gu.mediaservice.lib.auth._
 import com.gu.mediaservice.lib.aws._
+import com.gu.mediaservice.lib.config.InstanceForRequest
 import com.gu.mediaservice.lib.formatting.printDateTime
 import com.gu.mediaservice.lib.logging.{LogMarker, MarkerMap}
 import com.gu.mediaservice.lib.metadata.SoftDeletedMetadataTable
@@ -43,7 +44,7 @@ class MediaApi(
                 ws: WSClient,
                 authorisation: Authorisation,
                 embedder: Embedder,
-)(implicit val ec: ExecutionContext) extends BaseController with MessageSubjects with ArgoHelpers with ContentDisposition {
+              )(implicit val ec: ExecutionContext) extends BaseController with MessageSubjects with ArgoHelpers with ContentDisposition with InstanceForRequest {
 
   private val gridClient: GridClient = GridClient(config.services)(ws)
 
@@ -281,7 +282,7 @@ class MediaApi(
       .recover{ case error => respondError(InternalServerError, "cannot-get", s"Cannot get soft-deleted metadata ${error}") }
   }
 
-  def downloadImageExport(imageId: String, exportId: String, width: Int) = auth.async { request =>
+  def downloadImageExport(imageId: String, exportId: String, width: Int) = auth.async { implicit request =>
     implicit val logMarker: LogMarker = MarkerMap(
       "requestType" -> "download-image-export",
       "requestId" -> RequestLoggingFilter.getRequestId(request),
@@ -405,7 +406,7 @@ class MediaApi(
     }
   }
 
-  def downloadOriginalImage(id: String) = auth.async { request =>
+  def downloadOriginalImage(id: String) = auth.async { implicit request =>
     implicit val logMarker: LogMarker = MarkerMap(
       "requestType" -> "download-original-image",
       "requestId" -> RequestLoggingFilter.getRequestId(request),
@@ -433,7 +434,7 @@ class MediaApi(
     }
   }
 
-  def syndicateImage(id: String, partnerName: String, startPending: String) = auth.async { request =>
+  def syndicateImage(id: String, partnerName: String, startPending: String) = auth.async { implicit request =>
     implicit val logMarker: LogMarker = MarkerMap(
       "requestType" -> "syndicate-image",
       "requestId" -> RequestLoggingFilter.getRequestId(request),
@@ -456,7 +457,7 @@ class MediaApi(
     }
   }
 
-  def downloadOptimisedImage(id: String, width: Integer, height: Integer, quality: Integer) = auth.async { request =>
+  def downloadOptimisedImage(id: String, width: Integer, height: Integer, quality: Integer) = auth.async { implicit request =>
     implicit val logMarker: LogMarker = MarkerMap(
       "requestType" -> "download-optimised-image",
       "requestId" -> RequestLoggingFilter.getRequestId(request),
@@ -494,11 +495,11 @@ class MediaApi(
     user: String,
     partnerName: Option[String] = None,
     startPending: Option[String] = None,
-  )(implicit logMarker: LogMarker) = {
+  )(implicit logMarker: LogMarker, r: Request[AnyContent]) = {
 
     val baseRequest = ws.url(uri)
       .withHttpHeaders(Authentication.originalServiceHeaderName -> config.appName,
-        HttpHeaders.ORIGIN -> config.rootUri,
+        HttpHeaders.ORIGIN -> config.rootUri(instanceOf(r)),
         HttpHeaders.CONTENT_TYPE -> ContentType.APPLICATION_JSON.getMimeType)
 
     val request = onBehalfOfPrincipal(baseRequest)
