3c170ab3a5afde21601d085fce5c86f0eca880cb
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-11-21 21:35:11 +0000

    Push hardcoded S3 endpoint up to config. Sets up for use of non AWS S3 buckets. Allow S3 service endpoint to be set from config overriding s3.awazonaws.com.
    Allows buckets to be switched to a non AWS S3 provider in the future.

--- image-loader/app/model/Uploader.scala
+++ image-loader/app/model/Uploader.scala
@@ -63,7 +63,8 @@ case class ImageUploadOpsCfg(
   thumbQuality: Double,
   transcodedMimeTypes: List[MimeType],
   originalFileBucket: String,
-  thumbBucket: String
+  thumbBucket: String,
+  s3Endpoint: String
 )
 
 case class ImageUploadOpsDependencies(
@@ -91,7 +92,8 @@ object Uploader extends GridLogging {
       config.thumbQuality,
       config.transcodedMimeTypes,
       config.imageBucket,
-      config.thumbnailBucket
+      config.thumbnailBucket,
+      config.s3Endpoint
     )
   }
 
