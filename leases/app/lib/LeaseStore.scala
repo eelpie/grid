d7ead3649f4e112558603e34f836ed451971c1ff
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-11-23 13:49:24 +0000

    Move Leases to id + instance composite key; Scanomo had to be by past; seems to expect all keys to be actual fields of the object.
    Hope it doesn't check on reads!
    
    LeaseStore instance field is more Scanamo like.
    
    Imports
    
    Reduce Scope of Leases formatLeases to the bypassed put. May we why enums not working like main.

--- leases/app/lib/LeaseStore.scala
+++ leases/app/lib/LeaseStore.scala
@@ -1,46 +1,72 @@
 package lib
 
+import com.gu.mediaservice.model.Instance
 import com.gu.mediaservice.model.leases.{MediaLease, MediaLeaseType}
-import org.scanamo._
-import org.scanamo.syntax._
-import org.scanamo.generic.auto._
 import org.joda.time.DateTime
+import org.scanamo._
+import org.scanamo.generic.auto.{Typeclass, _}
+import org.scanamo.syntax._
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
 
 import scala.concurrent.{ExecutionContext, Future}
 
 
 class LeaseStore(tableName: String, client: DynamoDbAsyncClient) {
+
   implicit val dateTimeFormat: Typeclass[DateTime] =
     DynamoFormat.coercedXmap[DateTime, String, IllegalArgumentException](DateTime.parse, _.toString)
   implicit val enumFormat: Typeclass[MediaLeaseType] =
     DynamoFormat.coercedXmap[MediaLeaseType, String, IllegalArgumentException](MediaLeaseType(_), _.toString)
 
+  private val baseMediaLeaseFormat = DynamoFormat[MediaLease]
+
+  // 2. Define a custom format that also writes the instance index field
+  private def instanceAwareLeaseFormat(instance: Instance): DynamoFormat[MediaLease] = new DynamoFormat[MediaLease] {
+    override def read(dv: DynamoValue): Either[DynamoReadError, MediaLease] = baseMediaLeaseFormat.read(dv)
+
+    def write(l: MediaLease): DynamoValue = {
+      val baseValue = baseMediaLeaseFormat.write(l)
+      baseValue.asObject match {
+        case Some(obj) =>
+          val enrichedObj = obj + ("instance" -> DynamoValue.fromString(instance.id))
+          enrichedObj.toDynamoValue
+        case None =>
+          // Fallback for safety, though case classes always derive to Maps
+          baseValue
+      }
+    }
+  }
+
   private val leasesTable = Table[MediaLease](tableName)
 
-  def get(id: String)(implicit ec: ExecutionContext): Future[Option[MediaLease]] = {
-    ScanamoAsync(client).exec(leasesTable.get("id" === id)).map(_.flatMap(_.toOption))
+  def get(id: String)(implicit ec: ExecutionContext, instance: Instance): Future[Option[MediaLease]] = {
+    ScanamoAsync(client).exec(leasesTable.get("id" === id and "instance" === instance.id)).map(_.flatMap(_.toOption))
   }
 
-  def getForMedia(id: String)(implicit ec: ExecutionContext): Future[List[MediaLease]] = {
-    ScanamoAsync(client).exec(leasesTable.index("mediaId").query("mediaId" === id)).map(_.flatMap(_.toOption))
+  def getForMedia(id: String)(implicit ec: ExecutionContext, instance: Instance): Future[List[MediaLease]] = {
+    ScanamoAsync(client).exec(leasesTable.index("mediaId").query("mediaId" === id and "instance" === instance.id)).map(_.flatMap(_.toOption))
   }
 
-  def put(lease: MediaLease)(implicit ec: ExecutionContext) = {
-    ScanamoAsync(client).exec(leasesTable.put(lease))
+  def put(lease: MediaLease)(implicit ec: ExecutionContext, instance: Instance) = {
+    implicit val format: Typeclass[MediaLease] = instanceAwareLeaseFormat(instance)
+    val t = Table[MediaLease](tableName)
+    val op = t.put(lease)
+    ScanamoAsync(client).exec(op)
   }
 
-  def putAll(leases: List[MediaLease])(implicit ec: ExecutionContext) = {
-    ScanamoAsync(client).exec(leasesTable.putAll(leases.toSet))
+  def putAll(leases: List[MediaLease])(implicit ec: ExecutionContext, instance: Instance) = {
+    implicit val format: Typeclass[MediaLease] = instanceAwareLeaseFormat(instance)
+    val t = Table[MediaLease](tableName)
+    ScanamoAsync(client).exec(t.putAll(leases.toSet))
   }
 
-  def delete(id: String)(implicit ec: ExecutionContext) = {
-    ScanamoAsync(client).exec(leasesTable.delete("id" === id))
+  def delete(id: String)(implicit ec: ExecutionContext, instance: Instance) = {
+    ScanamoAsync(client).exec(leasesTable.delete("id" === id and "instance" === instance.id))
   }
 
-  def forEach[T](run: List[MediaLease] => T)(implicit ec: ExecutionContext) = ScanamoAsync(client).exec(
-    leasesTable.scan()
-      .map(ops => ops.flatMap(_.toOption))
+  def forEach[T](run: List[MediaLease] => T)(implicit ec: ExecutionContext, instance: Instance) = {
+    val instanceLeasesQuery = leasesTable.query("instance" === instance.id)
+    ScanamoAsync(client).exec(instanceLeasesQuery).map(ops => ops.flatMap(_.toOption))
       .map(run)
-  )
+  }
 }
