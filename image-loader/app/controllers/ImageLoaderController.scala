99147dce008d203c28731a00773b73e31d72d5e7
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-11-23 11:56:26 +0000

    Multi tenant vhosts - Redefine Service URLs to be a function of instance.
    All code which refers to a service URL needs to known which instance it is running as.
    Work through the usages.
    
    Switch service names to instance aware instance.domain vhost names.
    
    auth, kahuna and thrall move to vhost url. Stops at extends Panda. We are going to replace Panda though.
    
    loader, projection, usages, collections and leases move to vhost urls.
    XXX Forces migration to be instance aware to project images!
    
    cropper and metadata-editor are vhost based.
    
    imgproxy is vhost based.
    
    KahunaController will need to evaluate the service URLs to pass to the client on the fly.
    
    This is a different concern to the config class which provides the function for doing this.
    
    Catch all .rootUri usages in s strings in media-api.
    Media API will probably render contextual links now.
    s string substitutions allow alot of unevaluated functions to be rendered toString!

--- image-loader/app/controllers/ImageLoaderController.scala
+++ image-loader/app/controllers/ImageLoaderController.scala
@@ -1,10 +1,5 @@
 package controllers
 
-import org.apache.pekko.Done
-import org.apache.pekko.stream.Materializer
-import org.apache.pekko.stream.scaladsl.Source
-import software.amazon.awssdk.services.sqs.model.{Message => SQSMessage}
-import com.amazonaws.util.IOUtils
 import com.drew.imaging.ImageProcessingException
 import com.gu.mediaservice.GridClient
 import com.gu.mediaservice.lib.ImageIngestOperations.fileKeyFromId
@@ -13,19 +8,22 @@ import com.gu.mediaservice.lib.argo.model.Link
 import com.gu.mediaservice.lib.auth.Authentication.OnBehalfOfPrincipal
 import com.gu.mediaservice.lib.auth._
 import com.gu.mediaservice.lib.aws.{S3Ops, SimpleSqsMessageConsumer, SqsHelpers}
+import com.gu.mediaservice.lib.config.InstanceForRequest
 import com.gu.mediaservice.lib.formatting.printDateTime
 import com.gu.mediaservice.lib.logging.{FALLBACK, LogMarker, MarkerMap}
 import com.gu.mediaservice.lib.play.RequestLoggingFilter
 import com.gu.mediaservice.lib.{DateTimeUtils, ImageIngestOperations, ImageStorageProps}
-import com.gu.mediaservice.model.{UnsupportedMimeTypeException, UploadInfo}
-import org.scanamo.{ConditionNotMet, ScanamoError}
+import com.gu.mediaservice.model.{Instance, UnsupportedMimeTypeException, UploadInfo}
 import lib.FailureResponse.Response
-import lib.imaging.{MimeTypeDetection, NoSuchImageExistsInS3, UserImageLoaderException}
-import lib.storage.{ImageLoaderStore, S3FileDoesNotExistException}
 import lib._
+import lib.imaging.{MimeTypeDetection, NoSuchImageExistsInS3, UserImageLoaderException}
+import lib.storage.ImageLoaderStore
 import model.upload.UploadRequest
 import model.{Projector, QuarantineUploader, S3FileExtractedMetadata, S3IngestObject, StatusType, UploadStatus, UploadStatusRecord, UploadStatusUri, Uploader}
+import org.apache.pekko.Done
+import org.apache.pekko.stream.Materializer
 import org.joda.time.{DateTime, DateTimeZone}
+import org.scanamo.{ConditionNotMet, ScanamoError}
 import play.api.data.Form
 import play.api.data.Forms._
 import play.api.inject.ApplicationLifecycle
@@ -35,6 +33,7 @@ import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.cloudwatch.model.Dimension
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.{GetObjectRequest, HeadObjectRequest, NoSuchKeyException}
+import software.amazon.awssdk.services.sqs.model.{Message => SQSMessage}
 
 import java.io.{File, FileOutputStream}
 import java.net.URI
@@ -61,7 +60,7 @@ class ImageLoaderController(auth: Authentication,
                             metrics: ImageLoaderMetrics,
                             applicationLifecycle: ApplicationLifecycle)
                            (implicit val ec: ExecutionContext, materializer: Materializer)
-  extends BaseController with ArgoHelpers with SqsHelpers {
+  extends BaseController with ArgoHelpers with SqsHelpers with InstanceForRequest {
 
   private val AuthenticatedAndAuthorised = auth andThen authorisation.CommonActionFilters.authorisedForUpload
 
@@ -77,6 +76,9 @@ class ImageLoaderController(auth: Authentication,
     }
   })
 
+  //TODO first example of a non user facing message which needs to be made instance aware!
+  val maybeIngestQueueAndProcessor: Option[(SimpleSqsMessageConsumer, Future[Done])] = None
+  /*
   val maybeIngestQueueAndProcessor: Option[(SimpleSqsMessageConsumer, Future[Done])] = maybeIngestQueue.map { ingestQueue =>
     val processor = Source.repeat(())
       .mapAsyncUnordered(parallelism=1)(_ => {
@@ -116,26 +118,29 @@ class ImageLoaderController(auth: Authentication,
 
     (ingestQueue, processor)
   }
+  */
 
-  private lazy val indexResponse: Result = {
+  private def indexResponse(instance: Instance): Result = {
     val indexData = Map("description" -> "This is the Loader Service")
     val indexLinks = List(
-      Link("prepare", s"${config.rootUri}/prepare"),
-      Link("uploadStatus", s"${config.rootUri}/uploadStatus/{id}"),
-      Link("uploadStatuses", s"${config.rootUri}/uploadStatuses"),
-      Link("load", s"${config.rootUri}/images{?uploadedBy,identifiers,uploadTime,filename}"),
-      Link("import", s"${config.rootUri}/imports{?uri,uploadedBy,identifiers,uploadTime,filename}")
+      Link("prepare", s"${config.rootUri(instance)}/prepare"),
+      Link("uploadStatus", s"${config.rootUri(instance)}/uploadStatus/{id}"),
+      Link("uploadStatuses", s"${config.rootUri(instance)}/uploadStatuses"),
+      Link("load", s"${config.rootUri(instance)}/images{?uploadedBy,identifiers,uploadTime,filename}"),
+      Link("import", s"${config.rootUri(instance)}/imports{?uri,uploadedBy,identifiers,uploadTime,filename}")
     )
     respond(indexData, indexLinks)
   }
 
-  def index: Action[AnyContent] = AuthenticatedAndAuthorised { indexResponse }
-
-  private def quarantineOrStoreImage(uploadRequest: UploadRequest)(implicit logMarker: LogMarker) = {
-    quarantineUploader.map(_.quarantineFile(uploadRequest)).getOrElse(for { uploadStatusUri <- uploader.storeFile(uploadRequest)} yield{uploadStatusUri.toJsObject})
+  def index: Action[AnyContent] = AuthenticatedAndAuthorised { request =>
+    indexResponse(instanceOf(request))
   }
 
-  private def handleMessageFromIngestBucket(sqsMessage:SQSMessage)(basicLogMarker: LogMarker): Future[Unit] = Future[Future[Unit]]{
+  private def quarantineOrStoreImage(uploadRequest: UploadRequest)(implicit logMarker: LogMarker, instance: Instance) = {
+    quarantineUploader.map(_.quarantineFile(uploadRequest)(instance)).getOrElse(for { uploadStatusUri <- uploader.storeFile(uploadRequest)} yield{uploadStatusUri.toJsObject})
+  }
+
+  private def handleMessageFromIngestBucket(sqsMessage:SQSMessage)(basicLogMarker: LogMarker, request: Request[AnyContent]): Future[Unit] = Future[Future[Unit]]{
 
     logger.info(basicLogMarker, sqsMessage.toString)
 
@@ -187,7 +192,7 @@ class ImageLoaderController(auth: Authentication,
           }
           Future.unit
         } else {
-          attemptToProcessIngestedFile(s3IngestObject, isUiUpload)(logMarker) map { digestedFile =>
+          attemptToProcessIngestedFile(s3IngestObject, isUiUpload)(logMarker, request) map { digestedFile =>
             metrics.successfulIngestsFromQueue.incrementBothWithAndWithoutDimensions(metricDimensions)
             logger.info(logMarker, s"Successfully processed image ${digestedFile.file.getName}")
             store.deleteObjectFromIngestBucket(s3IngestObject.key)
@@ -205,7 +210,7 @@ class ImageLoaderController(auth: Authentication,
     }
   }.flatten
 
-  private def attemptToProcessIngestedFile(s3IngestObject:S3IngestObject, isUiUpload: Boolean)(initialLogMarker:LogMarker): Future[DigestedFile] = {
+  private def attemptToProcessIngestedFile(s3IngestObject:S3IngestObject, isUiUpload: Boolean)(initialLogMarker:LogMarker, request: Request[AnyContent]): Future[DigestedFile] = {
 
     logger.info(initialLogMarker, "Attempting to process file")
     val tempFile = createTempFile("s3IngestBucketFile")(initialLogMarker)
@@ -219,6 +224,7 @@ class ImageLoaderController(auth: Authentication,
       "mediaId" -> digestedFile.digest
     )
 
+    implicit val instance: Instance = instanceOf(request)
     val futureUploadStatusUri = uploadDigestedFileToStore(
         digestedFileFuture = Future(digestedFile),
         uploadedBy = s3IngestObject.uploadedBy,
@@ -272,7 +278,7 @@ class ImageLoaderController(auth: Authentication,
     .map(Ok(_))
   }
 
-  def loadImage(uploadedBy: Option[String], identifiers: Option[String], uploadTime: Option[String], filename: Option[String]): Action[DigestedFile] =  {
+  def loadImage(uploadedBy: Option[String], identifiers: Option[String], uploadTime: Option[String], filename: Option[String]): Action[DigestedFile] = {
     val uploadTimeToRecord = DateTimeUtils.fromValueOrNow(uploadTime)
 
     val initialContext = MarkerMap(
@@ -310,7 +316,9 @@ class ImageLoaderController(auth: Authentication,
           filename.flatMap(_.trim.nonEmptyOpt)
         )
         _ <- uploadStatusTable.setStatus(record)
-        result <- quarantineOrStoreImage(uploadRequest)
+
+        result <- quarantineOrStoreImage(uploadRequest)(context, instanceOf(req))
+
       } yield result
       result.onComplete( _ => Try { deleteTempFile(tempFile) } )
 
@@ -379,6 +387,7 @@ class ImageLoaderController(auth: Authentication,
     )
     val tempFile = createTempFile(s"projection-$imageId")(initialContext)
     auth.async { req =>
+      implicit val instance: Instance = instanceOf(req)
       implicit val context: LogMarker = initialContext ++ Map(
         "requestId" -> RequestLoggingFilter.getRequestId(req)
       )
@@ -433,7 +442,7 @@ class ImageLoaderController(auth: Authentication,
           identifiers.map(Json.parse(_).as[Map[String, String]]).getOrElse(Map.empty),
           uploadTime,
           filename
-      )
+      )(context, instanceOf(request))
 
       // under all circumstances, remove the temp files
       uploadResultFuture.onComplete { _ =>
@@ -460,7 +469,7 @@ class ImageLoaderController(auth: Authentication,
     identifiers: Map[String, String],
     uploadTime: Option[String],
     filename: Option[String]
-  )(implicit logMarker:LogMarker): Future[UploadStatusUri] = {
+  )(implicit logMarker:LogMarker, instance: Instance): Future[UploadStatusUri] = {
 
     for {
         digestedFile <- digestedFileFuture
@@ -575,7 +584,7 @@ class ImageLoaderController(auth: Authentication,
 
   private case class RestoreFromReplicaForm(imageId: String)
   def restoreFromReplica: Action[AnyContent] = AuthenticatedAndAuthorised.async { implicit request =>
-
+    implicit val instance: Instance = instanceOf(request)
     val imageId = Form(
       mapping(
         "imageId" -> text
@@ -637,7 +646,7 @@ class ImageLoaderController(auth: Authentication,
 
           future.map { _ =>
             logger.info(logMarker, s"Restored image $imageId from replica bucket $replicaBucket (key: $s3Key)")
-            Redirect(s"${config.kahunaUri}/images/$imageId")
+            Redirect(s"${config.kahunaUri(instance)}/images/$imageId")
           }
         case _ =>
           Future.successful(NotFound("Image not found in replica bucket"))
