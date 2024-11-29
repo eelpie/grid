70c9cc7354e1f286dcfbfe167407bd1edef370e4
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-11-29 16:04:26 +0000

    Push s3 end point to all s3 users. Bucket specific end points.
    Pull image and thumbnail buckets to common config.

--- image-loader/app/model/Uploader.scala
+++ image-loader/app/model/Uploader.scala
@@ -93,7 +93,7 @@ object Uploader extends GridLogging {
       config.transcodedMimeTypes,
       config.imageBucket,
       config.thumbnailBucket,
-      config.s3Endpoint
+      config.thumbnailBucketS3Endpoint
     )
   }
 
