7e6ca5b27d4f420fdb7e72f575cdea47e24a589c
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-12-15 17:24:25 +0000

    Introduce S3Bucket to document buckets with end points other than AWS.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/BaseStore.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/BaseStore.scala
@@ -1,7 +1,7 @@
 package com.gu.mediaservice.lib
 
 import org.apache.pekko.actor.{Cancellable, Scheduler}
-import com.gu.mediaservice.lib.aws.S3
+import com.gu.mediaservice.lib.aws.{S3, S3Bucket}
 import com.gu.mediaservice.lib.config.CommonConfig
 import com.gu.mediaservice.lib.logging.GridLogging
 import org.joda.time.DateTime
@@ -14,27 +14,25 @@ import scala.concurrent.duration._
 import scala.util.control.NonFatal
 
 
-abstract class BaseStore[TStoreKey, TStoreVal](bucket: String, config: CommonConfig, s3Endpoint: String)(implicit ec: ExecutionContext)
+abstract class BaseStore[TStoreKey, TStoreVal](bucket: S3Bucket, config: CommonConfig)(implicit ec: ExecutionContext)
   extends GridLogging {
-
-  def s3Endpoint: String
 
   val s3 = new S3(config)
 
   protected val store: AtomicReference[Map[TStoreKey, TStoreVal]] = new AtomicReference(Map.empty)
   protected val lastUpdated: AtomicReference[DateTime] = new AtomicReference(DateTime.now())
 
-  protected def getS3Object(key: String): Option[String] = s3.getObjectAsString(bucket, key, s3Endpoint)
+  protected def getS3Object(key: String): Option[String] = s3.getObjectAsString(bucket, key)
 
   protected def getLatestS3Stream: Option[InputStream] = {
-    val objects = s3.listObjects(bucket, s3Endpoint).getObjectSummaries.asScala
+    val objects = s3.listObjects(bucket).getObjectSummaries.asScala
       .filterNot(_.getKey == "AMAZON_SES_SETUP_NOTIFICATION")
 
     if (objects.nonEmpty) {
       val obj = objects.maxBy(_.getLastModified)
       logger.info(s"Latest key ${obj.getKey} in bucket $bucket")
 
-      val stream = s3.getObject(bucket, obj, s3Endpoint).getObjectContent
+      val stream = s3.getObject(bucket, obj).getObjectContent
       Some(stream)
     } else {
       logger.error(s"Bucket $bucket is empty")
