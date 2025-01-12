2b3d8359909d3607d45d1c5a889fc9a520d35951
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-01-12 20:47:53 +0000

    MB = 1024 * 1024.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/config/CommonConfig.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/config/CommonConfig.scala
@@ -59,7 +59,7 @@ abstract class CommonConfig(resources: GridConfigResources) extends AwsClientV1B
 
   val maybeBucketForUIUploads: Option[String] = maybeQuarantineBucket orElse maybeIngestBucket
 
-  val maybeUploadLimitInBytes: Option[Int] = intOpt("upload.limit.mb").map(_ * 1_000_000)
+  val maybeUploadLimitInBytes: Option[Int] = intOpt("upload.limit.mb").map(_ * 1024 * 1024)
 
   // Note: had to make these lazy to avoid init order problems ;_;
   val domainRoot: String = string("domain.root")
