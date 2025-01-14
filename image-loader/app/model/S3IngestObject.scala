792f7919ccdce3a32300ab4510fc201927e32144
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-01-14 23:52:46 +0000

    Set isFeedUpload field on uploaded and ingested images.
    Fix tests for isFeedUpload.

--- image-loader/app/model/S3IngestObject.scala
+++ image-loader/app/model/S3IngestObject.scala
@@ -14,7 +14,8 @@ case class S3IngestObject (
   uploadTime: java.util.Date,
   contentLength: Long,
   getInputStream: () => java.io.InputStream,
-  identifiers: Map[String, String] = Map.empty
+  identifiers: Map[String, String] = Map.empty,
+  isFeedUpload: Boolean,
 )
 
 object S3IngestObject {
@@ -26,18 +27,22 @@ object S3IngestObject {
     val s3Object = store.getS3Object(key)
     val metadata = s3Object.getObjectMetadata
 
+    val mediaIdFromUiUpload = metadata.getUserMetadata.asScala.get("media-id")
+    val isFeedUpload = mediaIdFromUiUpload.isEmpty  // TODO Not concise
+
     S3IngestObject(
       key,
       uploadedBy = keyParts.head,
       filename = keyParts.last,
-      maybeMediaIdFromUiUpload = metadata.getUserMetadata.asScala.get("media-id"), // set by the client in upload in manager.js
+      maybeMediaIdFromUiUpload = mediaIdFromUiUpload, // set by the client in upload in manager.js
       uploadTime = metadata.getLastModified,
       contentLength = metadata.getContentLength,
       getInputStream = () => s3Object.getObjectContent,
       identifiers = metadata.getUserMetadata.asScala.collect{
         case (key, value) if key.startsWith(ImageStorageProps.identifierMetadataKeyPrefix) =>
           key.stripPrefix(ImageStorageProps.identifierMetadataKeyPrefix) -> value
-      }.toMap
+      }.toMap,
+      isFeedUpload = isFeedUpload
     )
   }
 }
