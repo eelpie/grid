d3f10185ddb70668affe7071bd17169575a4f038
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-01-24 17:27:09 +0000

    Thumbs bucket has configurable end point.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/config/CommonConfig.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/config/CommonConfig.scala
@@ -80,8 +80,7 @@ abstract class CommonConfig(resources: GridConfigResources) extends AwsClientV1B
   val services = new SingleHostServices(domainRoot)
 
   val imageBucket: S3Bucket = S3Bucket(string("s3.image.bucket.name"), string("s3.image.bucket.endpoint"))
-  val thumbnailBucket: S3Bucket = S3Bucket(string("s3.thumb.bucket"), S3.AmazonAwsS3Endpoint)
-  val thumbnailBucketS3Endpoint: String = S3.AmazonAwsS3Endpoint
+  val thumbnailBucket: S3Bucket = S3Bucket(string("s3.thumb.bucket.name"), string("s3.thumb.bucket.endpoint"))
 
   val googleS3AccessKey = stringOpt("s3.accessKey")
   val googleS3SecretKey = stringOpt("s3.secretKey")
