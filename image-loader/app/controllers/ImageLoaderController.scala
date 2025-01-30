15611f1130668106761bf3d6f517a71ff5d6e0ff
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-01-30 08:27:31 +0000

    Usage event for prepare upload.

--- image-loader/app/controllers/ImageLoaderController.scala
+++ image-loader/app/controllers/ImageLoaderController.scala
@@ -298,9 +298,10 @@ class ImageLoaderController(auth: Authentication,
           errorMessage = None,
           expires = expiration.toEpochSecond, // TTL in case upload is never completed by client
           instance = instance.id
-        )).map(_ =>
+        )).map { _ =>
+          usageEvents.prepare(instance = instance, image = mediaId, user = uploadedBy)
           mediaId -> preSignedUrl
-        )
+        }
       }
     )
     .map(_.toMap)
