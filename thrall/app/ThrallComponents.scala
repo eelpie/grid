c0f506a80416d115389c60a7757172c32aeb4dba
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-03-30 11:40:06 +0100

    [reindexing] Reindex process queues project image operations.

--- thrall/app/ThrallComponents.scala
+++ thrall/app/ThrallComponents.scala
@@ -86,7 +86,8 @@ class ThrallComponents(context: Context) extends GridComponents(context, new Thr
     gridClient,
     auth,
     instanceMessageSender,
-    usageEvents
+    usageEvents,
+    messageSender
   )
 
   val thrallStreamProcessor = new ThrallStreamProcessor(
