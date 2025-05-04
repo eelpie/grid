6df5a9d20f11cd00ed0aa96320305ff5be20c114
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-05-04 16:13:01 +0100

    Logging.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/ImageOperations.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/ImageOperations.scala
@@ -213,7 +213,7 @@ class ImageOperations(playPath: String) extends GridLogging {
                       colourModel: Option[String],
                       orientationMetadata: Option[OrientationMetadata]
                      )(implicit logMarker: LogMarker): Future[(File, MimeType)] = {
-
+    val stopwatch = Stopwatch.start
 
     Future.successful {
       Vips.run { arena =>
@@ -240,6 +240,8 @@ class ImageOperations(playPath: String) extends GridLogging {
           VipsOption.Boolean("strip", true)
         )
       }
+
+      logger.info(addLogMarkers(stopwatch.elapsed), "Finished creating thumbnail")
       (outputFile, MimeType("image/jpeg"))
     }
   }
