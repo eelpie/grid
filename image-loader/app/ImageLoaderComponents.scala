b0f79cb5891da5169d1ae8f93c050519ad722b95
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-05-10 20:13:22 +0100

    ImageOperations users need to Vips.init().

--- image-loader/app/ImageLoaderComponents.scala
+++ image-loader/app/ImageLoaderComponents.scala
@@ -1,3 +1,4 @@
+import app.photofox.vipsffm.Vips
 import com.gu.mediaservice.GridClient
 import com.gu.mediaservice.lib.aws.{Bedrock, Embedder, S3Vectors, SimpleSqsMessageConsumer}
 import com.gu.mediaservice.lib.aws.{Bedrock, S3, S3Vectors, SimpleSqsMessageConsumer, Embedder}
@@ -25,7 +26,10 @@ class ImageLoaderComponents(context: Context) extends GridComponents(context, ne
   val store = new ImageLoaderStore(config)
   val maybeIngestQueue = config.maybeIngestSqsQueueUrl.map(queueUrl => new SimpleSqsMessageConsumer(queueUrl, config))
   val uploadStatusTable = new UploadStatusTable(config)
-  val imageOperations = new ImageOperations(context.environment.rootPath.getAbsolutePath)
+  val imageOperations = {
+    Vips.init()
+    new ImageOperations(context.environment.rootPath.getAbsolutePath)
+  }
   val notifications = new Notifications(config)
   val downloader = new Downloader()(ec,wsClient)
 
