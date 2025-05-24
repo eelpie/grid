06e754bb5b96b33388f7a348caa6d76470fe1a04
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-05-24 19:20:16 +0100

    [libvips] extract save Vimage to file.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/VipsImageOperations.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/VipsImageOperations.scala
@@ -234,6 +234,20 @@ class VipsImageOperations(playPath: String) extends GridLogging with ImageOperat
     } yield (outputFile, optimisedMimeType)
   }
 
+  private def saveImageToFile(image: VImage, qual: Double, outputFile: File): File = {
+    logger.info(s"Saving image to file: " + outputFile.getAbsolutePath)
+    image.jpegsave(outputFile.getAbsolutePath,
+      VipsOption.Int("Q", qual.toInt),
+      //VipsOption.Boolean("optimize-scans", true),
+      //VipsOption.Boolean("optimize-coding", true),
+      //VipsOption.Boolean("interlace", true),
+      //VipsOption.Boolean("trellis-quant", true),
+      // VipsOption.Int("quant-table", 3),
+      VipsOption.Boolean("strip", true)
+    )
+    outputFile
+  }
+
   // When a layered tiff is unpacked, the temp file (blah.something) is moved
   // to blah-0.something and contains the composite layer (which is what we want).
   // Other layers are then saved as blah-1.something etc.
