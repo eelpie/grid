9706417c2e073423994c2b06be8037e1d4df10a4
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-09-19 09:13:22 +0100

    Add syndicationStatus as a filter in the q parameter so that it can be selected from the UI.
    Order appearance matters.

--- media-api/app/lib/querysyntax/model.scala
+++ media-api/app/lib/querysyntax/model.scala
@@ -25,3 +25,4 @@ final case class DateRange(startDate: DateTime, endDate: DateTime) extends Value
 final case class HasValue(string: String) extends Value
 final case class IsValue(string: String) extends Value
 final case class SimilarValue(string: String) extends Value
+final case class SyndicationStatusValue(string: String) extends Value
