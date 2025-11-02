d2c75b7c1afd6072d6b2528dd69ad01b79a9aff4
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-11-02 10:44:42 +0000

    [multi-tenant] Refactor; group getMyInstances into Instances trait.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/instances/Instances.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/instances/Instances.scala
@@ -3,7 +3,7 @@ package com.gu.mediaservice.lib.instances
 import com.gu.mediaservice.lib.config.CommonConfig
 import com.gu.mediaservice.model.Instance
 import com.typesafe.scalalogging.StrictLogging
-import play.api.libs.json.Json
+import play.api.libs.json.{Json, Reads}
 import play.api.libs.ws.WSClient
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -17,10 +17,21 @@ trait Instances extends StrictLogging {
     wsClient.url(config.instancesEndpoint).get().map { r =>
       r.status match {
         case 200 =>
-          implicit val ir = Json.reads[Instance]
-          val instances = Json.parse(r.body).as[Seq[Instance]]
-          logger.info("Got instances: " + instances.map(_.id).mkString(","))
-          instances
+          implicit val ir: Reads[Instance] = Json.reads[Instance]
+          Json.parse(r.body).as[Seq[Instance]]
+        case _ =>
+          logger.warn("Got non 200 status for instances call: " + r.status)
+          Seq.empty
+      }
+    }
+  }
+
+  def getMyInstances(owner: String)(implicit ec: ExecutionContext): Future[Seq[Instance]] = {
+    wsClient.url(config.myInstancesEndpoint).withQueryStringParameters("owner" -> owner).get().map { r =>
+      r.status match {
+        case 200 =>
+          implicit val ir: Reads[Instance] = Json.reads[Instance]
+          Json.parse(r.body).as[Seq[Instance]]
         case _ =>
           logger.warn("Got non 200 status for instances call: " + r.status)
           Seq.empty
