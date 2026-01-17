062e8d463952cbc5f92aecfe67aa67570b39fd7e
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-01-17 22:22:21 +0000

    [libvips] Log toOptimisedFile time.

--- image-loader/app/model/upload/OptimiseOps.scala
+++ image-loader/app/model/upload/OptimiseOps.scala
@@ -1,9 +1,9 @@
 package model.upload
 
-import app.photofox.vipsffm.{VImage, Vips, VipsHelper}
+import app.photofox.vipsffm.VImage
 import com.gu.mediaservice.lib.ImageWrapper
-import com.gu.mediaservice.lib.imaging.{ImageOperations, VipsImageOperations}
-import com.gu.mediaservice.lib.logging.{LogMarker, MarkerMap}
+import com.gu.mediaservice.lib.imaging.VipsImageOperations
+import com.gu.mediaservice.lib.logging.{LogMarker, MarkerMap, Stopwatch}
 import com.gu.mediaservice.model.{MimeType, Png}
 
 import java.io.File
@@ -29,16 +29,18 @@ class OptimiseWithPngQuant(imageOperations: VipsImageOperations) extends Optimis
     )
 
     // Given a source file on any valid upload type, return a file of the optimised type
-    try {
-      val arena = Arena.ofConfined
+    Stopwatch("toOptimisedFile") {
+      try {
+        val arena = Arena.ofConfined
 
-      val image = VImage.newFromFile(arena, file.getAbsolutePath)
-      imageOperations.saveImageToFile(image: VImage, optimiseMimeType, 85, optimisedFile, quantise = true)
-      (optimisedFile, optimiseMimeType)
-    } catch {
-      case _: Exception =>
-        throw new Exception(s"Failed to optimise PNG file ${file.getAbsolutePath}")
-    }
+        val image = VImage.newFromFile(arena, file.getAbsolutePath)
+        imageOperations.saveImageToFile(image: VImage, optimiseMimeType, 85, optimisedFile, quantise = true)
+        (optimisedFile, optimiseMimeType)
+      } catch {
+        case _: Exception =>
+          throw new Exception(s"Failed to optimise PNG file ${file.getAbsolutePath}")
+      }
+    }(marker)
   }
 
   def shouldOptimise(mimeType: Option[MimeType]): Boolean = false
