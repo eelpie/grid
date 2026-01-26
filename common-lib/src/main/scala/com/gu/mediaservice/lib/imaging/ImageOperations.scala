715579bfc3d17bd1b3b860c86b606a0eeedbccba
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-01-26 19:55:53 +0000

    [libvips-cropping] createCrops moves to image operations for testing.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/ImageOperations.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/ImageOperations.scala
@@ -106,6 +106,28 @@ class ImageOperations(playPath: String) extends GridLogging {
     }
   }
 
+  def createCrops(sourceImage: VImage, dimensionList: List[Dimensions], imageId: String, bounds: Bounds, cropType: MimeType, tempDir: File, cropQuality: Int
+                 )(implicit logMarker: LogMarker, instance: Instance, arena: Arena): Seq[(File, String, Dimensions)] = {
+    Stopwatch(s"Resizing crops for ${imageId}") {
+      logger.info("Starting resizes")
+      val resizes = dimensionList.map { dimensions =>
+        val outputFile = File.createTempFile(s"resize-", s"${cropType.fileExtension}", tempDir) // TODO function for this
+
+        val file =resizeImageVips(sourceImage, dimensions, cropQuality, outputFile, cropType)
+
+        def outputFilename(imageId: String, bounds: Bounds, outputWidth: Int, fileType: MimeType, isMaster: Boolean = false, instance: Instance): String = {  // TODO push back to Crops
+          val masterString: String = if (isMaster) "master/" else ""
+          instance.id + "/" + s"$imageId/${Crop.getCropId(bounds)}/$masterString$outputWidth${fileType.fileExtension}"
+        }
+
+        val filename = outputFilename(imageId, bounds, dimensions.width, cropType, instance = instance)
+        (file, filename, dimensions)
+      }
+      logger.info("Done resizes")
+      resizes
+    }
+  }
+
   def resizeImageVips(
                        sourceImage: VImage,
                        dimensions: Dimensions,
