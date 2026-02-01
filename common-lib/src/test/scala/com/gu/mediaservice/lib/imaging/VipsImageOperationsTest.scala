00bcb0577df3c32840bbd10577df0035bf123ce7
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-02-01 12:20:19 +0000

    [libvips] Test for correct thumbnail of png with alpha.

--- common-lib/src/test/scala/com/gu/mediaservice/lib/imaging/VipsImageOperationsTest.scala
+++ common-lib/src/test/scala/com/gu/mediaservice/lib/imaging/VipsImageOperationsTest.scala
@@ -34,6 +34,18 @@ class VipsImageOperationsTest extends AnyFunSpec with Matchers with ScalaFutures
         r._1.isFile should be(true)
       }
     }
+
+    it("render PNG with alpha correctly") {
+      val image = fileAt("with-alpha.png")
+
+      val outputFile = new File("/Users/tony/Desktop/thumbnail-png-with-alpha.jpg")
+      val browserViewableImageImage = BrowserViewableImage("TODO", image, Tiff, Map.empty, false, Instance("TODO"))
+
+      val eventualThumbnail = new VipsImageOperations("").createThumbnail(browserViewableImageImage, 1000, 95, outputFile, None)
+      whenReady(eventualThumbnail) { r =>
+        r._1.isFile should be(true)
+      }
+    }
   }
 
   describe("identifyColourModel") {
