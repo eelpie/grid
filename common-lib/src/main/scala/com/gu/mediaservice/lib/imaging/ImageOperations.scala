7847c74e9511c94953fe5682379868377468a917
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-02-01 18:47:04 +0000

    [libvips-cropping] getImageInformation uses imageOperations.hasAlpha function

--- common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/ImageOperations.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/ImageOperations.scala
@@ -268,7 +268,7 @@ object ImageOperations extends GridLogging {
       var colourModel: Option[String] = None
       var colourModelInformation: Map[String, String] = Map.empty
 
-      val arena = Arena.ofConfined
+      implicit val arena: Arena = Arena.ofConfined
       try {
         val image = VImage.newFromFile(arena, sourceFile.getAbsolutePath)
 
@@ -293,7 +293,7 @@ object ImageOperations extends GridLogging {
         }
 
         colourModelInformation = Map {
-          "hasAlpha" -> image.hasAlpha.toString // TODO push to imageoperations for testing
+          "hasAlpha" -> hasAlpha(image).toString
         }
       } catch {
         case e: Exception =>
