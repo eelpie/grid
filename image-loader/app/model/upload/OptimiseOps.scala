0e16bdcf6e9d10cbf2ae09cd3d2f441e4f80cdf2
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-02-12 21:47:52 +0000

    [libvips-cropping] Show that it is possible to manually trim metadata without relying in strip on save.
    Leads to been able to set metadata on crops.
    If not removed, orientation will express itself on save if not removed even though it's not in a exif prefixed field.

--- image-loader/app/model/upload/OptimiseOps.scala
+++ image-loader/app/model/upload/OptimiseOps.scala
@@ -56,7 +56,7 @@ class OptimiseWithPngQuant(imageOperations: ImageOperations) extends OptimiseOps
           image
         }
 
-        imageOperations.saveImageToFile(correctedForICCProfile: VImage, optimiseMimeType, 85, optimisedFile, quantise = true)
+        imageOperations.saveImageToFile(correctedForICCProfile: VImage, optimiseMimeType, 85, optimisedFile, quantise = true, strip = true)
         (optimisedFile, optimiseMimeType)
       } catch {
         case _: Exception =>
