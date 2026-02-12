7ed8711a00d88013b2b8d8e7f0b86902b2724f5e
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-02-12 21:49:02 +0000

    [libvips-cropping] While manually stripping metadata might be useful, vips strip with keep for XMP feels safer.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/ImageOperations.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/ImageOperations.scala
@@ -1,7 +1,8 @@
 package com.gu.mediaservice.lib.imaging
 
 import app.photofox.vipsffm.enums.{VipsIntent, VipsInterpretation}
-import app.photofox.vipsffm.{VImage, VipsHelper, VipsOption}
+import app.photofox.vipsffm.jextract.VipsRaw
+import app.photofox.vipsffm.{VBlob, VImage, VipsHelper, VipsOption}
 import com.adobe.internal.xmp.options.SerializeOptions
 import com.adobe.internal.xmp.{XMPConst, XMPMetaFactory}
 import com.gu.mediaservice.lib.BrowserViewableImage
@@ -77,15 +78,14 @@ class ImageOperations(playPath: String) extends GridLogging {
       cropped
     }
 
-    val striped = manuallyStripMetadata(correctedForICCProfile)
-
     // Apply crop metadata
     // https://developers.google.com/search/docs/appearance/structured-data/image-license-metadata#iptc-photo-metadata
     makeXmpBlog(metadata).foreach { xmpBlob =>
-      VipsHelper.image_set_blob_copy(arena, striped.getUnsafeStructAddress, "xmp-data", VBlob.newFromBytes(arena, xmpBlob).getUnsafeDataAddress, xmpBlob.length)
+      logger.info("Tagging master crop with XMP metadata: " + new String(xmpBlob))
+      VipsHelper.image_set_blob_copy(arena, correctedForICCProfile.getUnsafeStructAddress, "xmp-data", VBlob.newFromBytes(arena, xmpBlob).getUnsafeDataAddress, xmpBlob.length)
     }
 
-    striped
+    correctedForICCProfile
   }
 
   private def makeXmpBlog(metadata: ImageMetadata): Option[Array[Byte]] = {
@@ -116,23 +116,6 @@ class ImageOperations(playPath: String) extends GridLogging {
     }
   }
 
-  private def manuallyStripMetadata(stripped: VImage): VImage = {
-    stripped.remove("exif-data")
-    stripped.remove("iptc-data")
-    stripped.remove("xmp-data")
-    stripped.remove("icc-profile-data")
-    stripped.remove("jpeg-thumbnail-data")
-    stripped.remove("orientation")
-
-    stripped.getFields.asScala.foreach { field =>
-      if (field.startsWith("exif-")) {
-        stripped.remove(field)
-      }
-    }
-    stripped
-  }
-
-
   // Updates metadata on existing file
   def appendMetadata(sourceFile: File, metadata: ImageMetadata): Future[File] = {
     runExiftoolCmd(
@@ -152,7 +135,7 @@ class ImageOperations(playPath: String) extends GridLogging {
     val scale = dimensions.width.toDouble / sourceDimensions.width.toDouble
     val resized = sourceImage.resize(scale)
 
-    saveImageToFile(resized, fileType, quality, outputFile, quantise = true, strip = true)
+    saveImageToFile(resized, fileType, quality, outputFile, quantise = true, keep = Some(VipsRaw.VIPS_FOREIGN_KEEP_XMP))
   }
 
   private def orient(op: IMOperation, orientationMetadata: Option[OrientationMetadata]): IMOperation = {
@@ -201,7 +184,7 @@ class ImageOperations(playPath: String) extends GridLogging {
           thumbnail
         }
         logger.info("Created thumbnail: " + rotated.getWidth + "x" + rotated.getHeight)
-        saveImageToFile(rotated, Jpeg, qual.toInt, outputFile, strip = true)
+        saveImageToFile(rotated, Jpeg, qual.toInt, outputFile)
 
         val thumbDimensions = Some(Dimensions(rotated.getWidth, rotated.getHeight))
         arena.close()
@@ -222,8 +205,9 @@ class ImageOperations(playPath: String) extends GridLogging {
     }
   }
 
-  def saveImageToFile(image: VImage, mimeType: MimeType, quality: Int, outputFile: File, strip: Boolean, quantise: Boolean = false): File = {
+  def saveImageToFile(image: VImage, mimeType: MimeType, quality: Int, outputFile: File, quantise: Boolean = false, keep: Option[Int] = None): File = {
     logger.info(s"Saving image as $mimeType to file: " + outputFile.getAbsolutePath)
+    val k = keep.getOrElse(VipsRaw.VIPS_FOREIGN_KEEP_NONE)
     mimeType match {
       case Jpeg =>
         image.jpegsave(outputFile.getAbsolutePath,
@@ -233,7 +217,8 @@ class ImageOperations(playPath: String) extends GridLogging {
           //VipsOption.Boolean("interlace", true),
           //VipsOption.Boolean("trellis-quant", true),
           // VipsOption.Int("quant-table", 3),
-          VipsOption.Boolean("strip", strip)
+          VipsOption.Boolean("strip", true),
+          VipsOption.Int("keep", k)
         )
         outputFile
 
@@ -245,13 +230,14 @@ class ImageOperations(playPath: String) extends GridLogging {
             VipsOption.Int("Q", quality),
             VipsOption.Int("effort", 1),
             //VipsOption.Int("compression", 6),
-            VipsOption.Int("bitdepth", 8),
-            VipsOption.Boolean("strip", true)
+            VipsOption.Boolean("strip", true),
+            VipsOption.Int("keep", k)
           )
         } else {
           image.pngsave(outputFile.getAbsolutePath,
             //VipsOption.Int("compression", 6),
-            VipsOption.Boolean("strip", true)
+            VipsOption.Boolean("strip", true),
+            VipsOption.Int("keep", k)
           )
         }
         outputFile
