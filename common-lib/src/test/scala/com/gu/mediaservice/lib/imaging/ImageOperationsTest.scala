5f2b679821c69dbd19c03e391d1985dfd1a8a656
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-02-16 12:04:28 +0000

    Testing render LAB colour spaces with alpha correctly in sRGB

--- common-lib/src/test/scala/com/gu/mediaservice/lib/imaging/ImageOperationsTest.scala
+++ common-lib/src/test/scala/com/gu/mediaservice/lib/imaging/ImageOperationsTest.scala
@@ -89,6 +89,18 @@ class ImageOperationsTest extends AnyFunSpec with Matchers with ScalaFutures {
       }
     }
 
+    it("render LAB colour spaces with alpha correctly in sRGB") {
+      val image = fileAt("lab8-with-alpha.tif")
+
+      val outputFile = new File("/Users/tony/Desktop/out4.jpg")
+      val browserViewableImageImage = BrowserViewableImage("TODO", image, Tiff, Map.empty, false, Instance("TODO"))
+
+      val eventualThumbnail = new ImageOperations("").createThumbnailVips(browserViewableImageImage, 1000, 95, outputFile, None)
+      whenReady(eventualThumbnail) { r =>
+        r._1.isFile should be(true)
+      }
+    }
+
     it("render Heif correctly") {
       val image = fileAt("IMG_0128.HEIC")
 
