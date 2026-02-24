98ade71fa81dd448016747c01184995c3ee9d3f8
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-02-24 07:44:10 +0000

    [libvips-cropping] Missing arena.close()

--- common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/ImageOperations.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/ImageOperations.scala
@@ -295,6 +295,7 @@ object ImageOperations extends GridLogging {
       } catch {
         case e: Exception =>
           logger.error("Error during getImageInformation", e)
+          arena.close()
           throw e
       }
       arena.close()
