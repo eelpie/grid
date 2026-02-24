9c6a15e2dc17a4b81da5f4f4f94e7ec59db3bb8f
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-02-24 22:10:36 +0000

    Image projector is bucket S3 endpoint aware.

--- image-loader/app/ImageLoaderComponents.scala
+++ image-loader/app/ImageLoaderComponents.scala
@@ -1,5 +1,6 @@
 import com.gu.mediaservice.GridClient
 import com.gu.mediaservice.lib.aws.{Bedrock, Embedder, S3Vectors, SimpleSqsMessageConsumer}
+import com.gu.mediaservice.lib.aws.{Bedrock, S3, S3Vectors, SimpleSqsMessageConsumer, Embedder}
 import com.gu.mediaservice.lib.imaging.ImageOperations
 import com.gu.mediaservice.lib.logging.GridLogging
 import com.gu.mediaservice.lib.play.GridComponents
@@ -39,7 +40,8 @@ class ImageLoaderComponents(context: Context) extends GridComponents(context, ne
     }
 
   val uploader = new Uploader(store, config, imageOperations, notifications, maybeEmbedder, imageProcessor, gridClient, auth)
-  val projector = Projector(config, imageOperations, imageProcessor, auth, maybeEmbedder)
+  val s3 = new S3(config)
+  val projector = Projector(config, imageOperations, imageProcessor, auth, maybeEmbedder, s3)
   val quarantineUploader: Option[QuarantineUploader] = config.maybeQuarantineBucket.map(_ =>
     new QuarantineUploader(new QuarantineStore(config), config)
   )
