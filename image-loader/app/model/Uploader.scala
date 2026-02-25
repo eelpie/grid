179224b15ed5aeabc4ebf0b5a31ae203c8aff889
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-02-25 11:25:57 +0000

    Setting up for GCP and AWS clients by introducing s3 end point along side bucket.
    Bucket should be (bucket, endPoint, client?)

--- image-loader/app/model/Uploader.scala
+++ image-loader/app/model/Uploader.scala
@@ -6,6 +6,8 @@ import com.gu.mediaservice.lib.Files.createTempFile
 import com.gu.mediaservice.lib.argo.ArgoHelpers
 import com.gu.mediaservice.lib.auth.Authentication
 import com.gu.mediaservice.lib.aws.{Embedder, EmbedderMessage, S3Object, UpdateMessage}
+import com.gu.mediaservice.lib.{BrowserViewableImage, ImageStorageProps, StorableOptimisedImage, StorableOriginalImage, StorableThumbImage}
+import com.gu.mediaservice.lib.aws.{Embedder, S3Bucket, S3Object, S3Vectors, UpdateMessage}
 import com.gu.mediaservice.lib.cleanup.ImageProcessor
 import com.gu.mediaservice.lib.formatting._
 import com.gu.mediaservice.lib.imaging.ImageOperations
@@ -67,9 +69,8 @@ case class ImageUploadOpsCfg(
   thumbWidth: Int,
   thumbQuality: Double,
   transcodedMimeTypes: List[MimeType],
-  originalFileBucket: String,
-  thumbBucket: String,
-  s3Endpoint: String
+  originalFileBucket: S3Bucket,
+  thumbBucket: S3Bucket,
 )
 
 case class ImageUploadOpsDependencies(
@@ -97,7 +98,6 @@ object Uploader extends GridLogging {
       config.transcodedMimeTypes,
       config.imageBucket,
       config.thumbnailBucket,
-      config.thumbnailBucketS3Endpoint
     )
   }
 
