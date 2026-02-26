220f03bd83da1321e286ea55dcd592e0ef40f452
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-02-26 19:56:43 +0000

    Missing parameter.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/config/CommonConfig.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/config/CommonConfig.scala
@@ -67,7 +67,7 @@ abstract class CommonConfig(resources: GridConfigResources) extends AwsClientV1B
     S3Bucket(failBucket, failBucketEndpoint, usesPathStyleURLs = booleanOpt("s3.fail.bucket.pathStyleURLs").getOrElse(false))
   }
 
-  val maybeQuarantineBucket: Option[S3Bucket] = stringOpt("s3.quarantine.bucket").map(S3Bucket(_, S3.AmazonAwsS3Endpoint))
+  val maybeQuarantineBucket: Option[S3Bucket] = stringOpt("s3.quarantine.bucket").map(S3Bucket(_, S3.AmazonAwsS3Endpoint, usesPathStyleURLs = booleanOpt("s3.fail.bucket.pathStyleURLs").getOrElse(false)))
 
   val maybeBucketForUIUploads: Option[S3Bucket] = maybeQuarantineBucket orElse maybeIngestBucket
 
