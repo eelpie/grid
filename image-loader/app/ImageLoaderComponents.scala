8d9b109203e5c66e6ac532cdeeea5eb54e20e255
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-02-28 17:03:05 +0000

    Ingest images from instance specific top level folders in the ingest bucket.
     Conflicts:
    	image-loader/app/controllers/ImageLoaderController.scala

--- image-loader/app/ImageLoaderComponents.scala
+++ image-loader/app/ImageLoaderComponents.scala
@@ -48,7 +48,7 @@ class ImageLoaderComponents(context: Context) extends GridComponents(context, ne
   val metrics = new ImageLoaderMetrics(config, actorSystem, applicationLifecycle)
 
   val controller = new ImageLoaderController(
-    auth, downloader, store, maybeIngestQueue, uploadStatusTable, notifications, config, uploader, quarantineUploader, projector, controllerComponents, gridClient, authorisation, metrics, applicationLifecycle)
+    auth, downloader, store, maybeIngestQueue, uploadStatusTable, config, uploader, quarantineUploader, projector, controllerComponents, gridClient, authorisation, metrics, wsClient, applicationLifecycle)
   val uploadStatusController = new UploadStatusController(auth, uploadStatusTable, config, controllerComponents, authorisation)
   val imageLoaderManagement = new ImageLoaderManagement(controllerComponents, buildInfo, controller.maybeIngestQueueAndProcessor)
 
