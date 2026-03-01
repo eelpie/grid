203193a3887336097934f553f415764a6d53ac54
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-03-01 21:40:11 +0000

    Observer wants 5:3 crop options.

--- kahuna/public/js/util/constants/cropOptions.js
+++ kahuna/public/js/util/constants/cropOptions.js
@@ -1,7 +1,7 @@
 // `ratioString` is sent to the server, being `undefined` for `freeform` is expected 🙈
-export const landscapeOld = {key: 'landscape OLD', ratio: 5 / 3, ratioString: '5:3', isHidden: true, displayName: "Landscape"};
+export const landscapeOld = {key: 'landscape OLD', ratio: 5 / 3, ratioString: '5:3', displayName: "Landscape 5:3"};
 export const landscape32 = {key: 'landscape32', ratio: 3 / 2, ratioString: '3:2', displayName: "landscape 3:2"};
-export const landscape = {key: 'landscape', ratio: 5 / 4, ratioString: '5:4', displayName: 'Landscape'};
+export const landscape = {key: 'landscape', ratio: 5 / 4, ratioString: '5:4', isHidden: true, displayName: 'Landscape'};
 export const portrait = {key: 'portrait', ratio: 4 / 5, ratioString: '4:5', displayName: 'Portrait'};
 export const video = {key: 'video', ratio: 16 / 9, ratioString: '16:9', displayName: 'Video'};
 export const square = {key: 'square', ratio: 1, ratioString: '1:1', displayName: 'Square'};
