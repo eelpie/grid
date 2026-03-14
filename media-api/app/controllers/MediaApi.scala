13840db2b2e07b30de6f2af7e962549a3b956c7d
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-03-14 22:14:45 +0000

    Initial instance awareness. Prepare upload records a request specific instance field in the upload status table.
    Add instance field to ImageMessage so thrall knows the instance of an uploaded image to index.
    
    Very nice light touch in image uploader but
    extends UpdateMessage forces this to be dealt with for all update points immediately.
    
    Push instance onto all compiler flagged UpdateMessage usages.
    
    # Conflicts:
    #	image-loader/app/model/Uploader.scala

--- media-api/app/controllers/MediaApi.scala
+++ media-api/app/controllers/MediaApi.scala
@@ -339,7 +339,7 @@ class MediaApi(
       "imageId" -> id,
     ) ++ RequestLoggingFilter.loggablePrincipal(request.user)
 
-    elasticSearch.getImageById(id) map {
+    elasticSearch.getImageById(id) map {  // TODO with instance!
       case Some(image) if isVisibleToAccessor(request.user, image) =>
         val imageCanBeDeleted = imageResponse.canBeDeleted(image)
 
@@ -347,7 +347,7 @@ class MediaApi(
           val canDelete = authorisation.isUploaderOrHasPermission(request.user, image.uploadedBy, DeleteImagePermission)
 
           if (canDelete) {
-            val updateMessage = UpdateMessage(subject = DeleteImage, id = Some(id))
+            val updateMessage = UpdateMessage(subject = DeleteImage, id = Some(id), instance = instanceOf(request).id)
             messageSender.publish(updateMessage)
             Accepted
           } else {
@@ -384,7 +384,8 @@ class MediaApi(
                   softDeletedMetadata = Some(SoftDeletedMetadata(
                     deleteTime = DateTime.now(DateTimeZone.UTC),
                     deletedBy = request.user.accessor.identity
-                  ))
+                  )),
+                  instance = instanceOf(request).id
                 )
               )
             }
@@ -417,7 +418,8 @@ class MediaApi(
             messageSender.publish(
               UpdateMessage(
                 subject = UnSoftDeleteImage,
-                id = Some(id)
+                id = Some(id),
+                instance = instanceOf(request).id
               )
             )
           }.map { _ => Accepted }
