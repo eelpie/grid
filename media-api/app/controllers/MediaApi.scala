f01bec1d498d23ddb9596a464d4ad6cc9a228b49
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-03-14 22:14:45 +0000

    Initial instance awareness. Prepare upload records a request specific instance field in the upload status table.
    Add instance field to ImageMessage so thrall knows the instance of an uploaded image to index.
    
    Very nice light touch in image uploader but
    extends UpdateMessage forces this to be dealt with for all update points immediately.
    
    Push instance onto all compiler flagged UpdateMessage usages.

--- media-api/app/controllers/MediaApi.scala
+++ media-api/app/controllers/MediaApi.scala
@@ -352,7 +352,7 @@ class MediaApi(
       "imageId" -> id,
     ) ++ RequestLoggingFilter.loggablePrincipal(request.user)
 
-    elasticSearch.getImageById(id) map {
+    elasticSearch.getImageById(id) map {  // TODO with instance!
       case Some(image) if isVisibleToAccessor(request.user, image) =>
         val imageCanBeDeleted = imageResponse.canBeDeleted(image)
 
@@ -360,7 +360,7 @@ class MediaApi(
           val canDelete = authorisation.isUploaderOrHasPermission(request.user, image.uploadedBy, DeleteImagePermission)
 
           if (canDelete) {
-            val updateMessage = UpdateMessage(subject = DeleteImage, id = Some(id))
+            val updateMessage = UpdateMessage(subject = DeleteImage, id = Some(id), instance = instanceOf(request).id)
             messageSender.publish(updateMessage)
             Accepted
           } else {
@@ -397,7 +397,8 @@ class MediaApi(
                   softDeletedMetadata = Some(SoftDeletedMetadata(
                     deleteTime = DateTime.now(DateTimeZone.UTC),
                     deletedBy = request.user.accessor.identity
-                  ))
+                  )),
+                  instance = instanceOf(request).id
                 )
               )
             }
@@ -434,7 +435,8 @@ class MediaApi(
           _ = messageSender.publish(
             UpdateMessage(
               subject = UnSoftDeleteImage,
-              id = Some(id)
+              id = Some(id),
+              instance = instanceOf(request).id
             )
           )
         } yield Accepted
