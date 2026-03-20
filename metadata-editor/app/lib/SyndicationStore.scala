0a7fe650e9a7da5c7a05791eb110fad735ed2ecd
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-03-20 18:52:00 +0000

    Meta data edits and syndication dynamo calls move to instance aware.
    Metadata edits are probably instance isolated now.

--- metadata-editor/app/lib/SyndicationStore.scala
+++ metadata-editor/app/lib/SyndicationStore.scala
@@ -1,8 +1,8 @@
 package lib
 
-import com.gu.mediaservice.lib.aws.DynamoDB
+import com.gu.mediaservice.lib.aws.InstanceAwareDynamoDB
 import com.gu.mediaservice.model.SyndicationRights
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 
 class SyndicationStore(client2: DynamoDbClient, tableName: String)
-  extends DynamoDB[SyndicationRights](client2, tableName)
+  extends InstanceAwareDynamoDB[SyndicationRights](client2, tableName)
