49bc4b3dd7d8a3f6f40f1d2c0000a7a2dc02110c
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-03-28 16:24:35 +0000

    No v1 client.

--- metadata-editor/app/lib/EditsStore.scala
+++ metadata-editor/app/lib/EditsStore.scala
@@ -4,4 +4,4 @@ import com.gu.mediaservice.lib.aws.InstanceAwareDynamoDB
 import com.gu.mediaservice.model.Edits
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 
-class EditsStore(client2: DynamoDbClient, tableName: String) extends InstanceAwareDynamoDB[Edits](client, client2, tableName, Some(Edits.LastModified))
+class EditsStore(client2: DynamoDbClient, tableName: String) extends InstanceAwareDynamoDB[Edits](client2, tableName, Some(Edits.LastModified))
