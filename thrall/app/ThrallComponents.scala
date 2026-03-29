b8e1498b5689445d7b35d05c69a6358f4d3aba0d
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-03-29 12:20:52 +0100

    Delete InnerServiceStatusCheckController
    "checks connectivity to all other internal services..." which sounds like something we can let the container orchestrator handle.
    InnerServiceStatusCheckController was the only user of Services.allInternalUris

--- thrall/app/ThrallComponents.scala
+++ thrall/app/ThrallComponents.scala
@@ -1,7 +1,6 @@
 import com.gu.kinesis.{KinesisRecord, KinesisSource, ConsumerConfig => KclPekkoStreamConfig}
 import com.gu.mediaservice.GridClient
 import com.gu.mediaservice.lib.aws.{S3Ops, S3Vectors, ThrallMessageSender}
-import com.gu.mediaservice.lib.management.InnerServiceStatusCheckController
 import com.gu.mediaservice.lib.metadata.SoftDeletedMetadataTable
 import com.gu.mediaservice.lib.play.GridComponents
 import com.typesafe.scalalogging.StrictLogging
@@ -87,7 +86,6 @@ class ThrallComponents(context: Context) extends GridComponents(context, new Thr
   val thrallController = new ThrallController(es, store, migrationSourceWithSender.send, messageSender, actorSystem, auth, config.services, controllerComponents, gridClient)
   val reaperController = new ReaperController(es, store, s3Vectors, authorisation, config, actorSystem.scheduler, maybeCustomReapableEligibility, softDeletedMetadataTable, thrallMetrics, auth, config.services, controllerComponents)
   val healthCheckController = new HealthCheck(es, streamRunning.isCompleted, config, controllerComponents)
-  val InnerServiceStatusCheckController = new InnerServiceStatusCheckController(auth, controllerComponents, config.services, wsClient)
 
-  override lazy val router = new Routes(httpErrorHandler, thrallController, reaperController, healthCheckController, management, InnerServiceStatusCheckController, assets)
+  override lazy val router = new Routes(httpErrorHandler, thrallController, reaperController, healthCheckController, management, assets)
 }
