4222763f2e220b7cff3ece1e48b2ca663c09048d
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-05-04 16:42:41 +0100

    Media API's Elastic responses are probably instance specific.
    Media API's ElasticSearch class accepts instances inputs into current index alias functions.
    
    Make imagesCurrentAlias and imagesMigrationAlias a function of instance; gives instances specific indexes with a high degree of compiler checked confidence.
    
    Elastic tests compile in instance aware form.
    
    Instance specific index name during Elasticsearch tests.

--- media-api/app/controllers/MediaApi.scala
+++ media-api/app/controllers/MediaApi.scala
@@ -186,6 +186,7 @@ class MediaApi(
   }
 
   def uploadedBy(id: String) = auth.async { request =>
+    implicit val instance: Instance = instanceOf(request)
     implicit val logMarker: LogMarker = MarkerMap(
       "requestType" -> "uploaded-by",
       "requestId" -> RequestLoggingFilter.getRequestId(request),
@@ -261,6 +262,7 @@ class MediaApi(
   }
 
   def getImageExport(imageId: String, exportId: String) = auth.async { request =>
+    implicit val instance: Instance = instanceOf(request)
     implicit val logMarker: LogMarker = MarkerMap(
       "requestType" -> "get-image-export",
       "requestId" -> RequestLoggingFilter.getRequestId(request),
@@ -323,13 +325,14 @@ class MediaApi(
   }
 
   def hardDeleteImage(id: String) = auth.async { request =>
+    implicit val instance: Instance = instanceOf(request)
     implicit val logMarker: LogMarker = MarkerMap(
       "requestType" -> "hard-delete-image",
       "requestId" -> RequestLoggingFilter.getRequestId(request),
       "imageId" -> id,
     ) ++ RequestLoggingFilter.loggablePrincipal(request.user)
 
-    elasticSearch.getImageById(id) map {  // TODO with instance!
+    elasticSearch.getImageById(id) map {
       case Some(image) if isVisibleToAccessor(request.user, image) =>
         val imageCanBeDeleted = imageResponse.canBeDeleted(image)
 
@@ -352,6 +355,7 @@ class MediaApi(
   }
 
   def deleteImage(id: String) = auth.async { request =>
+    implicit val instance: Instance = instanceOf(request)
     implicit val logMarker: LogMarker = MarkerMap(
       "requestType" -> "delete-image",
       "requestId" -> RequestLoggingFilter.getRequestId(request),
@@ -391,6 +395,7 @@ class MediaApi(
   }
 
   def unSoftDeleteImage(id: String) = auth.async { implicit request =>
+    implicit val instance: Instance = instanceOf(request)
     implicit val logMarker: LogMarker = MarkerMap(
       "requestType" -> "soft-undelete-image",
       "requestId" -> RequestLoggingFilter.getRequestId(request),
