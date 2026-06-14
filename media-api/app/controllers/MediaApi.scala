cb7d9184bc8baf451ed61f5539fde6a92167c555
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-06-14 12:13:07 +0100

    Happy with numCandiates to be 2 * k given we have a constraint on simalatiry in place.

--- media-api/app/controllers/MediaApi.scala
+++ media-api/app/controllers/MediaApi.scala
@@ -728,7 +728,7 @@ class MediaApi(
               query = semanticQuery,
               queryEmbedding = embedding,
               k = k,
-              numCandidates = Math.max(k * 2, 100),
+              numCandidates = k * 2,
               vecWeight = weight,
               filterOpt = filterOpt
             )
