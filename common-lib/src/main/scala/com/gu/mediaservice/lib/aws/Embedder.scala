ad334f7a612a3a83e7b9a8756647eaa3dadb7ac8
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-06-14 11:21:43 +0100

    createImageEmbedding is available on Embedder interface.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/aws/Embedder.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/aws/Embedder.scala
@@ -1,14 +1,10 @@
 package com.gu.mediaservice.lib.aws
 import com.amazonaws.services.sqs.model.SendMessageResult
 import com.gu.mediaservice.lib.logging.{GridLogging, LogMarker}
-import com.gu.mediaservice.model.{Jpeg, MimeType, Png, Tiff}
+import com.gu.mediaservice.model.ImageMetadata
 import play.api.libs.json.{Json, OFormat}
-import software.amazon.awssdk.services.s3vectors.model.QueryVectorsResponse
-import software.amazon.awssdk.services.s3vectors.model.{QueryOutputVector, QueryVectorsResponse, VectorData}
 
-import java.nio.file.{Files, Path}
 import scala.concurrent.{ExecutionContext, Future}
-import scala.jdk.CollectionConverters.CollectionHasAsScala
 
 case class EmbedderMessage(imageId: String, fileType: String, s3Bucket: String, s3Key: String, instance: String)
 
@@ -23,6 +19,11 @@ class Embedder(bedrock: Bedrock, sqs: SimpleSqsMessageConsumer)(implicit ec: Exe
     for {
       embedding <- bedrock.createTextEmbedding(query)
     } yield embedding
+  }
+
+  def createImageEmbedding(source: Array[Byte], maybeMetadata: Option[ImageMetadata])(implicit logMarker: LogMarker): Future[List[Float]] = {
+    logger.info(logMarker, s"Creating image embedding")
+    bedrock.createImageEmbeddings(source, maybeMetadata)
   }
 
   def queueImageToEmbed(message: EmbedderMessage)(implicit logMarker: LogMarker) = {
