47452e811bb3c835d715567db6e6a3b95d5d5420
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-06-28 21:35:41 +0100

    [send-embedder-sqs-message] Introduce the embedding source S3 bucket to hold formatted source images for embedding.
    If the embedder is configured,
    ImageUpload returns the formatted source image for embedding in addition to the uploaded Image.
    
    Writes the sources images to this new bucket.
    Added StorableEmbeddingSourceImage to follow the existing Uploader / Projected shared pattern.
    
    Emits the existing embedder SQS message but now
    references the formatted image in the embedding source bucket.
    
    Image loader logs the where it is configured to queue embedding requests to at startup.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/ImageOperations.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/ImageOperations.scala
@@ -216,7 +216,7 @@ class ImageOperations(playPath: String) extends GridLogging {
   def createEmbeddingSource(originalImageFile: File,
                             orientationMetadata: Option[OrientationMetadata],
                             embeddingSourceImageFormat: EmbeddingSourceImageFormat
-                           ): Future[Array[Byte]] = {
+                           ): Future[Option[Array[Byte]]] = {
     Future {
       val arena = Arena.ofConfined
 
@@ -255,7 +255,7 @@ class ImageOperations(playPath: String) extends GridLogging {
         val bytes = buffer.toByteArray
         val embeddingSource = bytes
         logger.info("Created embedding source with length: " + embeddingSource.length)
-        embeddingSource
+        Some(embeddingSource)
 
       } catch {
         case e: Throwable =>
