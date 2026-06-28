89eb6f766901dd3634859cf668043618102863e1
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-06-28 13:01:07 +0100

    [generalise-embedding-impl] Embedder takes a general EmbeddingImplementation rather than Bedrock specficilly.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/aws/Embedder.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/aws/Embedder.scala
@@ -1,13 +1,11 @@
 package com.gu.mediaservice.lib.aws
 import com.amazonaws.services.sqs.model.SendMessageResult
+import com.gu.mediaservice.lib.embeddings.EmbeddingImplementation
 import com.gu.mediaservice.lib.logging.{GridLogging, LogMarker}
 import com.gu.mediaservice.model.ImageMetadata
 import play.api.libs.json.{Json, OFormat}
-import software.amazon.awssdk.services.s3vectors.model.QueryVectorsResponse
-import software.amazon.awssdk.services.s3vectors.model.{QueryOutputVector, QueryVectorsResponse, VectorData}
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.jdk.CollectionConverters.CollectionHasAsScala
 
 case class EmbedderMessage(imageId: String, fileType: String, s3Bucket: String, s3Key: String, instance: String)
 
@@ -15,12 +13,12 @@ object EmbedderMessage {
   implicit val format: OFormat[EmbedderMessage] = Json.format[EmbedderMessage]
 }
 
-class Embedder(bedrock: Bedrock, sqs: SimpleSqsMessageConsumer)(implicit ec: ExecutionContext) extends GridLogging {
+class Embedder(embedding: EmbeddingImplementation, sqs: SimpleSqsMessageConsumer)(implicit ec: ExecutionContext) extends GridLogging {
 
   def createQueryEmbedding(query: String)(implicit logMarker: LogMarker): Future[List[Float]] = {
     logger.info(logMarker, s"Creating text embedding for query: $query")
     for {
-      embedding <- bedrock.createTextEmbedding(query)
+      embedding <- embedding.createTextEmbedding(query)
     } yield embedding
   }
 
