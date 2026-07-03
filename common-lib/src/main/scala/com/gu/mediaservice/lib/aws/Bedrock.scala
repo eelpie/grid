6e3717920871cadcc0adb649601803241402f367
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-07-03 08:29:22 +0100

    Port Bedrock create image embedding back from lambda. Bedrock implements EmbeddingImplementation

--- common-lib/src/main/scala/com/gu/mediaservice/lib/aws/Bedrock.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/aws/Bedrock.scala
@@ -1,16 +1,17 @@
 package com.gu.mediaservice.lib.aws
 
-import software.amazon.awssdk.services.bedrockruntime.model._
-import software.amazon.awssdk.services.bedrockruntime._
 import com.gu.mediaservice.lib.config.CommonConfig
-import play.api.libs.json.Json
-import software.amazon.awssdk.core.SdkBytes
-
-import java.net.URI
+import com.gu.mediaservice.lib.embeddings.{EmbeddingImplementation, EmbeddingSourceImageFormat}
 import com.gu.mediaservice.lib.logging.LogMarker
+import com.gu.mediaservice.model.{CohereV4Embedding, Embedding, ImageMetadata, Jpeg, MimeType}
+import org.apache.commons.codec.binary.Base64
 import play.api.libs.json.OFormat.oFormatFromReadsAndOWrites
 import play.api.libs.json._
+import software.amazon.awssdk.core.SdkBytes
+import software.amazon.awssdk.services.bedrockruntime._
+import software.amazon.awssdk.services.bedrockruntime.model._
 
+import java.net.URI
 import scala.concurrent.{ExecutionContext, Future}
 
 object Bedrock {
@@ -22,10 +23,19 @@ object Bedrock {
   )
 
   private implicit val bedrockTextRequestFormat: OFormat[BedrockTextRequest] = Json.format[BedrockTextRequest]
+
+  case class BedrockImageRequest(
+    input_type: String,
+    embedding_types: List[String],
+    images: List[String],
+    output_dimension: Int
+  )
+
+  private implicit val bedrockImageRequestFormat: OFormat[BedrockImageRequest] = Json.format[BedrockImageRequest]
 }
 
 class Bedrock(config: CommonConfig)
-  extends AwsClientV2BuilderUtils {
+  extends EmbeddingImplementation with AwsClientV2BuilderUtils {
 
   // TODO: figure out what the more usual pattern for turning off localstack behaviour is
   override def awsLocalEndpointUri: Option[URI] = None
@@ -37,13 +47,37 @@ class Bedrock(config: CommonConfig)
       .build()
   }
 
-  private def createRequestBody(inputData: String): InvokeModelRequest = {
+  private def createSearchQueryRequestBody(inputData: String): InvokeModelRequest = {
     val body = Bedrock.BedrockTextRequest(
       input_type = "search_query",
       embedding_types = List("float"),
       texts = List(inputData),
       output_dimension = 256
     )
+    val jsonBody = Json.toJson(body).toString()
+
+    val request: InvokeModelRequest = {
+      InvokeModelRequest
+        .builder()
+        .accept("*/*")
+        .body(SdkBytes.fromUtf8String(jsonBody))
+        .contentType("application/json")
+        .modelId("global.cohere.embed-v4:0")
+        .build()
+    }
+    request
+  }
+
+  private def createImageSearchDocumentRequestBody(base64Image: String, imageMimeType: MimeType): InvokeModelRequest = {
+    val body = Bedrock.BedrockImageRequest(
+      input_type = "search_document",
+      embedding_types = List("float"),
+      images = List(
+        s"`data:${imageMimeType.name};base64,$base64Image`"
+      ),
+      output_dimension = 1536
+    )
+
     val jsonBody = Json.toJson(body).toString()
 
     val request: InvokeModelRequest = {
@@ -77,7 +111,7 @@ class Bedrock(config: CommonConfig)
   }
 
   def createTextEmbedding(inputData: String)(implicit ec: ExecutionContext, logMarker: LogMarker): Future[List[Double]] = {
-    val requestBody = createRequestBody(inputData)
+    val requestBody = createSearchQueryRequestBody(inputData)
     val bedrockFuture = Future { sendBedrockEmbeddingRequest(requestBody) }
     bedrockFuture.map { response =>
       val responseBody = response.body().asUtf8String()
@@ -91,4 +125,32 @@ class Bedrock(config: CommonConfig)
       embedding
     }
   }
+
+  override def createImageEmbeddings(source: Array[Byte], mimeType: MimeType, maybeMetadata: Option[ImageMetadata])(implicit ec: ExecutionContext, logMarker: LogMarker): Future[Embedding] = {
+    val base64ImageData = Base64.encodeBase64String(source)
+    val requestBody = createImageSearchDocumentRequestBody(
+      base64ImageData, embeddingSourceImageFormat().format
+    )
+    val bedrockFuture = Future {
+      sendBedrockEmbeddingRequest(requestBody)
+    }
+    bedrockFuture.map { response =>
+      val responseBody = response.body().asUtf8String()
+      val json = Json.parse(responseBody)
+      // Extract the embedding array (first element since it's an array of arrays)
+      val embeddings = (json \ "embeddings" \ "float")(0).as[List[Double]]
+      logger.info(
+        logMarker,
+        s"Successfully created image embedding. Vector size: ${embeddings.size}"
+      )
+      embeddings
+    }.map { embeddings =>
+      Embedding(
+        cohereEmbedV4 = Some(CohereV4Embedding(embeddings))
+      )
+    }
+  }
+
+  override def embeddingSourceImageFormat(): EmbeddingSourceImageFormat = EmbeddingSourceImageFormat(longestAxis = 3000, format = Jpeg, letterBox = false)
+
 }
