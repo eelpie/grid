77e83d05c4f227f3d207246670f302b53bf508b5
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-07-05 15:46:40 +0100

    [query] Setting up to use similar too as boolean clause of normal search. Extract embedding for image lookup.

--- media-api/app/controllers/MediaApi.scala
+++ media-api/app/controllers/MediaApi.scala
@@ -9,10 +9,6 @@ import com.gu.mediaservice.lib.auth.Permissions.{ArchiveImages, DeleteCropsOrUsa
 import com.gu.mediaservice.lib.auth._
 import com.gu.mediaservice.lib.auth.provider.ApiKeyAuthenticationProvider
 import com.gu.mediaservice.lib.aws._
-import com.gu.mediaservice.lib.aws.{ContentDisposition, Embedder, ThrallMessageSender, UpdateMessage}
-import com.gu.mediaservice.lib.aws.{ContentDisposition, S3, ThrallMessageSender, UpdateMessage}
-import com.gu.mediaservice.lib.aws.{ContentDisposition, ThrallMessageSender, UpdateMessage}
-import com.gu.mediaservice.lib.aws.{ContentDisposition, S3, ThrallMessageSender, UpdateMessage}
 import com.gu.mediaservice.lib.config.InstanceForRequest
 import com.gu.mediaservice.lib.events.UsageEvents
 import com.gu.mediaservice.lib.formatting.printDateTime
@@ -26,6 +22,7 @@ import com.sksamuel.elastic4s.requests.searches.queries.Query
 import lib._
 import lib.elasticsearch._
 import lib.querysyntax.Condition
+import lib.querysyntax.{Match, SimilarField, SimilarValue}
 import org.apache.http.entity.ContentType
 import org.apache.pekko.stream.scaladsl.StreamConverters
 import org.http4s.UriTemplate
@@ -688,6 +685,7 @@ class MediaApi(
         )
 
         val filterOpt = buildAiFilter(parts.filterConditions, params)
+        val eventualMaybeEmbedding = embeddingForImageId(imageId)
 
         // Compute the filtered pool total and ticker count badges in parallel with the
         // KNN search, so similar-image results show the same "Best k of N matches" total
@@ -696,15 +694,11 @@ class MediaApi(
 
         for {
           maybeImage <- elasticSearch.getImageById(imageId)
-          maybeEmbedding = maybeImage
-            .filter(image => isVisibleToAccessor(request.user, image))
-            .flatMap(_.embedding)
-            .flatMap(_.geminiEmbedding2)
-            .map(_.image)
+          maybeEmbedding <- eventualMaybeEmbedding
           searchResults <- maybeEmbedding match {
             // If we have an embedding, perform the KNN search. If not, return an empty result set.
             case Some(embedding) =>
-              elasticSearch.semanticSearch(embedding, k = k, numCandidates = Math.max(k * 2, 100), filterOpt = filterOpt)
+              elasticSearch.knnSearch(embedding, k = k, numCandidates = Math.max(k * 2, 100), filterOpt = filterOpt)
             case None =>
               Future.successful(SearchResults(Nil, total = 0, extraCounts = None))
           }
@@ -803,13 +797,37 @@ class MediaApi(
       } else {
         _searchParams
       }
-      SearchParams.validate(searchParams).fold(
+
+      val x: Future[Result] = SearchParams.validate(searchParams).fold(
         // TODO: respondErrorCollection?
         errors => Future.successful(respondError(UnprocessableEntity, InvalidUriParams.errorKey,
           errors.map(_.message).mkString(", "))
-        ),
-        params => performSearchAndRespond(params)
+        ), { params: SearchParams =>
+          // Extract the similar parameter from the structured query
+          val maybeSimilarImageId = params.structuredQuery.flatMap {
+            case Match(SimilarField, SimilarValue(imageId)) =>
+                logger.info("Saw similar parameter: " + imageId)
+                Some(imageId)
+            case _ => None
+          }.headOption
+
+          performSearchAndRespond(params)
+        }
       )
+      x
+    }
+  }
+
+  private def embeddingForImageId(imageId: String)(implicit logMarker: LogMarker, instance: Instance, request: Authentication.Request[AnyContent]): Future[Option[List[Float]]] = {
+    for {
+      maybeImage <- elasticSearch.getImageById(imageId)
+      maybeEmbedding: Option[List[Float]] = maybeImage
+        .filter(image => isVisibleToAccessor(request.user, image))
+        .flatMap(_.embedding)
+        .flatMap(_.geminiEmbedding2)
+        .map(_.image.map(_.toFloat))
+    } yield {
+      maybeEmbedding
     }
   }
 
