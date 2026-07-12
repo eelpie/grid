f97cdfbf18d15eb8a558f5eb65cf24ba75cc0563
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-07-12 10:59:04 +0100

    Drop fileType from EmbedderMessage; it is already past on the S3 object metadata.
    S3ImageStorage.storeImage sets S3.store
    metadata.setContentType(m.name)

--- image-loader/app/model/Uploader.scala
+++ image-loader/app/model/Uploader.scala
@@ -487,7 +487,6 @@ class Uploader(
       _ = imageUpload.embeddingSource.foreach { embeddingSource =>
         queueImageToEmbed(EmbedderMessage(
           uploadRequest.imageId,
-          Png.fileExtension,
           config.embeddingSourceBucket.bucket,
           config.embeddingSourceBucket.keyFromS3URL(embeddingSource.uri),
           instance.id
