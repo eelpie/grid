ab65848c5e7ae5921f1ce9910c0b56e9b4de6720
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-07-18 20:41:47 +0100

    [libvips] thumbnails flushes thumbnail through png save to make in memory copy while preserving metadata.
    [libvips] Use a large tall thumbnail to expose out of order read which exceeds the line cache

--- common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/VipsImageOperations.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/VipsImageOperations.scala
@@ -187,11 +187,14 @@ class VipsImageOperations(playPath: String) extends GridLogging with ImageOperat
           VipsOption.Enum("intent", VipsIntent.INTENT_PERCEPTUAL),
           VipsOption.String("export-profile", "srgb")
         )
+
+        val inMemoryCopy = VImage.pngloadBuffer(arena, thumbnail.pngsaveBuffer(VipsOption.Int("compression", 0)))
+
         val rotated = orientationMetadata.map(_.orientationCorrection()).map { angle =>
           logger.info("Rotating thumbnail: " + angle)
-          thumbnail.rotate(angle)
+          inMemoryCopy.rotate(angle)
         }.getOrElse {
-          thumbnail
+          inMemoryCopy
         }
         logger.info("Created thumbnail: " + rotated.getWidth + "x" + rotated.getHeight)
         saveImageToFile(rotated, Jpeg, qual.toInt, outputFile)
