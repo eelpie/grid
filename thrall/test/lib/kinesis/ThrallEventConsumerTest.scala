2801614fbfc588c0480c8d2e8af7522d7de50d94
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-07-23 19:45:30 +0100

    Media API's Elastic responses are instance specific.
    Media API's ElasticSearch class accepts instances inputs into current index alias functions.
    
    Make imagesCurrentAlias and imagesMigrationAlias a function of instance; gives instances specific indexes with a high degree of compiler checked confidence.
    
    Elastic tests compile in instance aware form.
    
    Instance specific index name during Elasticsearch tests.

--- thrall/test/lib/kinesis/ThrallEventConsumerTest.scala
+++ thrall/test/lib/kinesis/ThrallEventConsumerTest.scala
@@ -1,13 +1,16 @@
 package lib.kinesis
 
-import helpers.Fixtures
+import com.gu.mediaservice.model.Instance
+import lib.elasticsearch.ElasticSearchTestBase
 import org.scalatest.EitherValues
-import org.scalatest.freespec.AnyFreeSpec
-import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.mockito.MockitoSugar
 
+import java.util.UUID
 
-class ThrallEventConsumerTest extends AnyFreeSpec with Matchers with Fixtures with MockitoSugar with EitherValues {
+class ThrallEventConsumerTest extends ElasticSearchTestBase with MockitoSugar with EitherValues {
+
+  override val instance: Instance = Instance(UUID.randomUUID().toString)
+
   "parse message" - {
     "parse minimal message" in {
       val j =
@@ -20,7 +23,7 @@ class ThrallEventConsumerTest extends AnyFreeSpec with Matchers with Fixtures wi
           |}
           |""".stripMargin.getBytes()
       val m2 = ThrallEventConsumer.parseRecord(j, java.time.Instant.EPOCH)
-      m2.isRight shouldEqual true
+      m2.isRight shouldEqual (true)
       m2.value.subject shouldBe "DeleteImageMessage"
     }
   }
