3ff045353f5531d06480b7c0b244bfc97539bf53
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-07-24 12:46:54 +0100

    [generalise-embedding-impl] Extract common embedding implementation interface.
    embeddingSourceImageFormat so that embedder implementations can describe their required input format.

---
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/embeddings/EmbeddingImplementation.scala
@@ -0,0 +1,14 @@
+package com.gu.mediaservice.lib.embeddings
+
+import com.gu.mediaservice.lib.logging.LogMarker
+import com.gu.mediaservice.model.{ImageMetadata, Jpeg, MimeType}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+trait EmbeddingImplementation {
+  def createImageEmbeddings(source: Array[Byte], maybeMetadata: Option[ImageMetadata])(implicit ec: ExecutionContext, logMarker: LogMarker): Future[List[Float]]
+  def createTextEmbedding(query: String)(implicit ec: ExecutionContext, logMarker: LogMarker): Future[List[Double]]
+  def embeddingSourceImageFormat(): EmbeddingSourceImageFormat
+}
+
+case class EmbeddingSourceImageFormat(longestAxis: Int, format: MimeType = Jpeg, letterBox: Boolean)
