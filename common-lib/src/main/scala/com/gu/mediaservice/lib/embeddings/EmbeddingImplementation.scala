aa365052602f4c2a37a73a0e75f38291ccd0223b
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-07-24 14:19:12 +0100

    [generalise-embedding-impl] EmbeddingImplementation returns a full Embedding object rather than List[Float] to push the specific embedding field choice down to the implementations.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/embeddings/EmbeddingImplementation.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/embeddings/EmbeddingImplementation.scala
@@ -1,12 +1,12 @@
 package com.gu.mediaservice.lib.embeddings
 
 import com.gu.mediaservice.lib.logging.LogMarker
-import com.gu.mediaservice.model.{ImageMetadata, Jpeg, MimeType}
+import com.gu.mediaservice.model.{Embedding, ImageMetadata, Jpeg, MimeType}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 trait EmbeddingImplementation {
-  def createImageEmbeddings(source: Array[Byte], mimeType: MimeType, maybeMetadata: Option[ImageMetadata])(implicit ec: ExecutionContext, logMarker: LogMarker): Future[List[Float]]
+  def createImageEmbeddings(source: Array[Byte], mimeType: MimeType, maybeMetadata: Option[ImageMetadata])(implicit ec: ExecutionContext, logMarker: LogMarker): Future[Embedding]
   def createTextEmbedding(query: String)(implicit ec: ExecutionContext, logMarker: LogMarker): Future[List[Double]]
   def embeddingSourceImageFormat(): EmbeddingSourceImageFormat
 }
