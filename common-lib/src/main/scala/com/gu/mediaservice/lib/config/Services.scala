efecb6e2b32dba379a0689bccd8f20a0af637438
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-07-25 19:30:29 +0100

    Regression.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/config/Services.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/config/Services.scala
@@ -46,7 +46,7 @@ protected class SingleHostServices(val domain: String) extends Services {
 
   override def loaderBaseUri(instance: Instance): String = vhostServiceName("image-loader", instance)
 
-  override def projectionBaseUri(instance: Instance): String = loaderBaseUri(instance)
+  override def projectionBaseUri(instance: Instance): String = vhostServiceName("projection", instance)
 
   override def cropperBaseUri(instance: Instance): String = vhostServiceName("cropper", instance)
 
