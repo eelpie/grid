64c007736697845cf9a6192a6437d0eff8e51748
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-07-26 12:10:17 +0100

    Provide media api config hooks for tuning aiSearchMinimumSimilarity and aiSimilarImagesMinimumSimilarity.

--- media-api/app/lib/elasticsearch/ElasticSearch.scala
+++ media-api/app/lib/elasticsearch/ElasticSearch.scala
@@ -250,7 +250,7 @@ class ElasticSearch(
         .queryVector(queryEmbedding)
         .k(k)
         .numCandidates(numCandidates)
-        .similarity(0.40f)
+        .similarity(config.aiSearchMinimumSimilarity)
       )
       .size(k)
 
@@ -399,7 +399,7 @@ class ElasticSearch(
       .queryVector(queryEmbedding.map(_.toDouble))
       .k(k)
       .numCandidates(numCandidates)
-      .similarity(0.80f)
+      .similarity(config.aiSimilarImagesMinimumSimilarity)
   }
 
   def search(params: SearchParams, maybeSimilarToVector: Option[Seq[Float]] = None)(implicit ex: ExecutionContext, instance: Instance, logMarker:MarkerMap = MarkerMap()): Future[SearchResults] = {
