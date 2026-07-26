7f7eb8a7baaa9a473cb0234673e93ace7dcaccfa
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-07-26 17:27:15 +0100

    [persist-embeddings] createEmbeddingsSource moves to ImageUploadOpsDependencies so that Projector can no-op it.

--- image-loader/test/scala/model/ImageUploadTest.scala
+++ image-loader/test/scala/model/ImageUploadTest.scala
@@ -72,8 +72,9 @@ class ImageUploadTest extends AsyncFunSuite with Matchers with MockitoSugar {
       storeOrProjectOriginalFile = storeOrProjectOriginalFile,
       storeOrProjectThumbFile = storeOrProjectThumbFile,
       storeOrProjectOptimisedImage = storeOrProjectOptimisedPNG,
+      createEmbeddingsSource = (_, _, _) => Future.successful(None),
       storeEmbeddingSource = storeOrProjectEmbeddingSource,
-      maybeEmbedder = None
+      maybeEmbedder = None,
     )
 
     val tempFile = ResourceHelpers.fileAt(fileName)
