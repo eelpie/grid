5c60cb96fed1f1bdd6234bf9b8d35d4bb674b326
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-08-13 13:10:22 +0100

    Extract S3 URL to key onto S3Bucket.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/aws/S3Bucket.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/aws/S3Bucket.scala
@@ -13,6 +13,11 @@ case class S3Bucket(name: String, endPoint: String, usesPathStyleURLs: Boolean, 
     new URI("http", bucketBaseURL.getHost, bucketBaseURL.getPath + key, null)
   }
 
+  def keyFromURL(url: URI): String = {
+    // get path and remove leading `/`
+    url.getPath.drop(1)
+  }
+
   private def bucketURL(): URI = {
     if (usesPathStyleURLs) {
       new URI("https", endPoint, s"/$name/", null)
