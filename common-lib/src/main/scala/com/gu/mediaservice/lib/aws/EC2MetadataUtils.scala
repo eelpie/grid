b0caf968fcd94d40d4103792e6c63038ad90092f
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-08-27 19:37:53 +0100

    Nerf EC2MetadataUtils.getInstanceId which has no meaning in a containerised world.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/aws/EC2MetadataUtils.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/aws/EC2MetadataUtils.scala
@@ -6,8 +6,6 @@ import scala.util.Using
 
 object EC2MetadataUtils {
 
-  def getInstanceId: Option[String] = Using(Ec2MetadataClient.create()) { client =>
-    client.get("/latest/meta-data/instance-id").asString()
-  }.toOption
+  def getInstanceId: Option[String] = None
 
 }
