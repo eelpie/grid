4f2d92439048ce48a618ab9e74ef836158ffc454
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-08-28 22:16:49 +0100

    imageBucket moves up to CommonConfig to reduce duplication of bucket init.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/config/CommonConfig.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/config/CommonConfig.scala
@@ -70,6 +70,8 @@ abstract class CommonConfig(resources: GridConfigResources) extends AwsClientBui
 
   lazy val softDeletedMetadataTable: String = string("dynamo.table.softDelete.metadata")
 
+  val imageBucket: String = string("s3.image.bucket")
+
   val maybeIngestSqsQueueUrl: Option[String] = stringOpt("sqs.ingest.queue.url")
   val maybeIngestBucket: Option[String] = stringOpt("s3.ingest.bucket")
   val maybeFailBucket: Option[String] = stringOpt("s3.fail.bucket")
