5daff1fa5b8fc9d2f3449992420a0ea2a625a5a8
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-08-29 13:13:40 +0100

    Clean up; misleading parameter name.

--- media-api/app/controllers/MediaApi.scala
+++ media-api/app/controllers/MediaApi.scala
@@ -44,7 +44,7 @@ class MediaApi(
                 imageResponse: ImageResponse,
                 config: MediaApiConfig,
                 override val controllerComponents: ControllerComponents,
-                s3Client: S3, // TODO rename to s3
+                s3: S3,
                 mediaApiMetrics: MediaApiMetrics,
                 ws: WSClient,
                 authorisation: Authorisation,
@@ -325,7 +325,7 @@ class MediaApi(
           export <- source.exports.find(_.id.contains(exportId))
           asset <- export.assets.find(_.dimensions.exists(_.width == width))
           key = config.imgPublishingBucket.keyFromURL(asset.file)
-          s3Res = Try(s3Client.getObject(config.imgPublishingBucket, key))
+          s3Res = Try(s3.getObject(config.imgPublishingBucket, key))
           _ = s3Res.failed.foreach { ex =>
             logger.error("Failed to fetch S3 object", ex)
           }
@@ -463,7 +463,7 @@ class MediaApi(
         logger.info(logMarker, s"Download original image: $id from user: ${Authentication.getIdentity(request.user)}")
         mediaApiMetrics.incrementImageDownload(apiKey, mediaApiMetrics.OriginalDownloadType)
         val key = config.imageBucket.keyFromURL(image.source.file)
-        val s3Object = s3Client.getObject(config.imageBucket, key)
+        val s3Object = s3.getObject(config.imageBucket, key)
         val file = StreamConverters.fromInputStream(() => s3Object)
         val entity = HttpEntity.Streamed(file, image.source.size, image.source.mimeType.map(_.name))
 
@@ -528,7 +528,7 @@ class MediaApi(
 
         val key = config.imageBucket.keyFromURL(image.optimisedPng.getOrElse(image.source).file)
         val sourceImageUri =
-          new URI(s3Client.signUrl(config.imageBucket, key, image, imageType = image.optimisedPng match {
+          new URI(s3.signUrl(config.imageBucket, key, image, imageType = image.optimisedPng match {
             case Some(_) => OptimisedPng
             case _ => Source
           }))
