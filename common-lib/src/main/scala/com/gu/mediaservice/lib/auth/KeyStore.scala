dd23a509b5c3895530a1bff2463ab1d635a42a00
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-08-31 20:15:04 +0100

    Log number of KeyStore objects found.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/auth/KeyStore.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/auth/KeyStore.scala
@@ -21,6 +21,7 @@ class KeyStore(bucket: S3Bucket, config: CommonConfig, s3: S3)(implicit ec: Exec
 
   private def fetchAll: Map[String, ApiAccessor] = {
     val objects = Await.result(s3.list(bucket, ""), 10.seconds)
+    logger.info(s"fetchAll found ${objects.size} objects")
     val keys = objects.map( s3Object => bucket.keyFromURL(s3Object.uri))
     keys.flatMap(k => getS3Object(k).map(k -> ApiAccessor(_))).toMap
   }
