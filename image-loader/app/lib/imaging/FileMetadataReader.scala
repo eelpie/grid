cbee4933d7b4df0e582316fb74b9d2806dc49fdc
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-09-03 22:05:03 +0100

    Remove runIdentifyCmd and it's unused upsteams fromIPTCHeadersWithColorInfo and fromIPTCHeadersWithColorInfo.

--- image-loader/app/lib/imaging/FileMetadataReader.scala
+++ image-loader/app/lib/imaging/FileMetadataReader.scala
@@ -67,10 +67,7 @@ object FileMetadataReader extends GridLogging {
       metadata <- readMetadata(image)
       colourModelInformation <- getColorModelInformation(image, metadata, mimeType)
     }
-    yield getMetadataWithIPTCHeaders(metadata, imageId).copy(
-      colourModelInformation = colourModelInformation,
-      c2pa = if (C2paDetector.hasC2paManifest(image, mimeType)) FileMetadata.C2paAvailable else FileMetadata.NoC2PA
-    )
+    yield getMetadataWithIPTCHeaders(metadata, imageId).copy(colourModelInformation = colourModelInformation)
 
   private def getMetadataWithIPTCHeaders(metadata: Metadata, imageId:String): FileMetadata =
     FileMetadata(
@@ -185,19 +182,6 @@ object FileMetadataReader extends GridLogging {
   }
 
   private def dateToUTCString(date: DateTime): String = ISODateTimeFormat.dateTime.print(date.withZone(DateTimeZone.UTC))
-
-  def getColorModelInformation(image: File, metadata: Metadata, mimeType: MimeType)(implicit logMarker: LogMarker): Future[Map[String, String]] = {
-    val stopWatch = Stopwatch.start
-    val source = addImage(image)
-
-    val formatter = format(source)("%r")
-
-    runIdentifyCmd(formatter, useImageMagick = false).map { imageType => getColourInformation(metadata, imageType.headOption, mimeType) }
-      .recover { case _ => getColourInformation(metadata, None, mimeType) }.map { result =>
-      logger.info(addLogMarkers(stopWatch.elapsed), "Finished getColorModelInformation")
-      result
-    }
-  }
 
   // bits per sample might be a useful value, eg. "1", "8"; or it might be annoying like "1 bits/component/pixel", "8 8 8 bits/component/pixel"
   // either way we want everything up to the first space
