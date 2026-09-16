b9b6deac826e3876cb3daf236544bc33100d767b
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-09-16 21:08:30 +0100

    [libvips] toInMemoryCopy uses copyMemory.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/VipsImageOperations.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/VipsImageOperations.scala
@@ -188,7 +188,7 @@ class VipsImageOperations(playPath: String) extends GridLogging with ImageOperat
           VipsOption.String("export-profile", "srgb")
         )
 
-        val inMemoryCopy = VImage.pngloadBuffer(arena, thumbnail.pngsaveBuffer(VipsOption.Int("compression", 0)))
+        val inMemoryCopy = thumbnail.copyMemory()
 
         val rotated = orientationMetadata.map(_.orientationCorrection()).map { angle =>
           logger.info("Rotating thumbnail: " + angle)
@@ -345,4 +345,19 @@ object VipsImageOperations extends GridLogging {
     }
   }
 
+  def hasAlpha(image: VImage)(implicit arena: Arena): Boolean = image.hasAlpha
+
+  def isGraphicVips(image: VImage)(implicit arena: Arena): Boolean = {
+    val numberOfBands = VipsHelper.image_get_bands(image.getUnsafeStructAddress)
+   logger.info("Number of bands: " + numberOfBands)
+    // Indexed plus alpha would be 2 bands
+
+    val format = VipsHelper.image_get_format(image.getUnsafeStructAddress)
+    logger.info("Format: " + format)
+
+    val paletteType = VipsHelper.image_get_typeof(arena, image.getUnsafeStructAddress, "palette")
+    logger.info("Palette type: " + paletteType)
+
+    paletteType > 0 || numberOfBands < 3
+  }
 }
