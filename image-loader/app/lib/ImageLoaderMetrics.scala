62b14921a5a26a80af4fa715213510a078566162
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-09-20 11:47:04 +0100

    [opentel-metrics] image loader dual write.

--- image-loader/app/lib/ImageLoaderMetrics.scala
+++ image-loader/app/lib/ImageLoaderMetrics.scala
@@ -1,15 +1,40 @@
 package lib
 
+import io.opentelemetry.api.metrics.LongCounter
+import io.opentelemetry.sdk.OpenTelemetrySdk
 import org.apache.pekko.actor.ActorSystem
 import com.gu.mediaservice.lib.metrics.CloudWatchMetrics
 import play.api.inject.ApplicationLifecycle
+import software.amazon.awssdk.services.cloudwatch.model.Dimension
 
 class ImageLoaderMetrics(config: ImageLoaderConfig, actorSystem: ActorSystem, applicationLifecycle: ApplicationLifecycle)
     extends CloudWatchMetrics (namespace = s"${config.stage}/ImageLoader", config, actorSystem, applicationLifecycle){
 
-  val successfulIngestsFromQueue = new CountMetric("SuccessfulIngestsFromQueue")
+  private val meter = ImageLoaderMetrics.openTelemetrySdk.getMeter("image-loader")
 
-  val failedIngestsFromQueue = new CountMetric("FailedIngestsFromQueue")
+  // Count metrics which are dual written to CloudWatch and to an OpenTelemetry counter.
+  private class DualWrittenCountMetric(name: String, openTelemetryName: String) extends CountMetric(name) {
+    private val openTelemetryCounter: LongCounter = meter.counterBuilder(openTelemetryName).build()
 
-  val abandonedMessagesFromQueue = new CountMetric("AbandonedMessagesFromQueue")
+    override def increment(dimensions: List[Dimension] = Nil, n: Long = 1): Unit = {
+      super.increment(dimensions, n)
+      openTelemetryCounter.add(n)
+    }
+
+    override def incrementBothWithAndWithoutDimensions(dimensions: List[Dimension], n: Long = 1): Unit = {
+      super.incrementBothWithAndWithoutDimensions(dimensions, n)
+      openTelemetryCounter.add(n)
+    }
+  }
+
+  val successfulIngestsFromQueue: CountMetric = new DualWrittenCountMetric("SuccessfulIngestsFromQueue", "ingest.queue.successful")
+
+  val failedIngestsFromQueue: CountMetric = new DualWrittenCountMetric("FailedIngestsFromQueue", "ingest.queue.failed")
+
+  val abandonedMessagesFromQueue: CountMetric = new DualWrittenCountMetric("AbandonedMessagesFromQueue", "ingest.queue.abandoned")
+}
+
+object ImageLoaderMetrics {
+  import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk
+  private val openTelemetrySdk: OpenTelemetrySdk = AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk
 }
